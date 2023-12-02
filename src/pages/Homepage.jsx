4d68dc269f6e27405b31f51984ddda7e7c8d7fbe
FrontEndDevo <mylinkacco@gmail.com>
2023-12-02 12:52:48 +0200

    Create a test component and import it in Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,9 @@
+import TestComponent from "../components/TestComponent/TestComponent";
+
 const Homepage = () => {
   return (
     <>
-      <h1>Component 1 in Homepage</h1>
-      <h1>Component 2</h1>
-      <h1>Component 3</h1>
-      <h1>Component 4</h1>
+      <TestComponent />
     </>
   );
 };
