273fd963f1d83ee8188b0b5fd6042ee4f9bbc815
FrontEndDevo <mylinkacco@gmail.com>
2023-12-02 13:51:08 +0200

    Install React Slick for sliders

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
