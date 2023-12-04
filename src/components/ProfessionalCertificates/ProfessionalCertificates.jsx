77d1e6fb6ba3ae062adbe4482c6c8b6e8412a54d
FrontEndDevo <mylinkacco@gmail.com>
2023-12-04 16:09:49 +0200

    Add the primary BG color to tailwind.config.js and complete styling the text in ProfessionalCertificates.jsx

--- src/components/ProfessionalCertificates/ProfessionalCertificates.jsx
+++ src/components/ProfessionalCertificates/ProfessionalCertificates.jsx
@@ -2,18 +2,25 @@ import { Link } from "react-router-dom";
 
 const ProfessionalCertificates = () => {
   return (
-    <section className="bg-gray-200">
+    <section className="bg-gray-200 py-20">
       <div className="container">
-        <div>
-          <h2>How Professional Certificates have helped others</h2>
-          <p>
+        <div className="mb-10 text-center">
+          <h2 className=" text-5xl mb-6">
+            How Professional Certificates have helped others
+          </h2>
+          <p className="text-2xl font-light">
             Stories from those that have completed Professional Certificates
           </p>
         </div>
 
         {/* slider */}
 
-        <Link to="/">View more customer story</Link>
+        <Link
+          to="/"
+          className="bg-primary text-xl py-2 px-6 rounded-full text-white font-bold"
+        >
+          View more customer story
+        </Link>
       </div>
     </section>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#00A5A5",
+      },
+    },
+
     container: {
       center: true,
     },
