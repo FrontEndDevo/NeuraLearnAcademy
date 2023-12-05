95467a5cbb79535e91cb01999d3761ab579c4946
FrontEndDevo <mylinkacco@gmail.com>
2023-12-06 00:22:06 +0200

    Finish the main title with its icon

--- src/components/ProfessionalCertificates/NewCareerRecommendations/NewCareerRecommendations.jsx
+++ src/components/ProfessionalCertificates/NewCareerRecommendations/NewCareerRecommendations.jsx
@@ -3,7 +3,9 @@ import course_two from "../../../assets/images/homepage/course_2.jpg";
 import course_three from "../../../assets/images/homepage/course_3.jpg";
 import course_four from "../../../assets/images/homepage/course_4.jpg";
 import course_five from "../../../assets/images/homepage/course_5.jpg";
+import smileIcon from "../../../assets/images/homepage/icon.png";
 const NewCareerRecommendations = () => {
+  // This is a temporary array of objects (courses) until fetch all of these from DB.
   const recomendedCourses = [
     {
       title: "The complete course of Bussines for beginners",
@@ -76,7 +78,21 @@ const NewCareerRecommendations = () => {
       review: 4.7,
     },
   ];
-  return <section>NewCareerRecommendations</section>;
+
+  return (
+    <section className="py-20 container">
+      <div className="flex items-center gap-4 mx-6 lg:mx-0">
+        <h2 className="font-bold text-xl lg:text-3xl">
+          Recommend this if you are looking for a new career path
+        </h2>
+        <img
+          src={smileIcon}
+          alt="smile icon"
+          className="w-10 h-10 lg:w-16 lg:h-16"
+        />
+      </div>
+    </section>
+  );
 };
 
 export default NewCareerRecommendations;
