d39ce58323d7823ec9442a70672f451e080ebff4
FrontEndDevo <mylinkacco@gmail.com>
2023-12-06 01:12:35 +0200

    Finish Responsive-Design for Slider in NewCareerRecommendations.jsx

--- src/components/NewCareerRecommendations/NewCareerRecommendations.jsx
+++ src/components/NewCareerRecommendations/NewCareerRecommendations.jsx
@@ -18,6 +18,31 @@ const settings = {
   autoplay: true,
   autoplaySpeed: 2000,
   pauseOnHover: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
 };
 
 const NewCareerRecommendations = () => {
