af30a581f79a18a14755341f276becd43259043b
FrontEndDevo <mylinkacco@gmail.com>
2023-12-05 23:57:48 +0200

    Prepare array of courses and the temporary images

--- src/components/ProfessionalCertificates/NewCareerRecommendations/NewCareerRecommendations.jsx
+++ src/components/ProfessionalCertificates/NewCareerRecommendations/NewCareerRecommendations.jsx
@@ -1,4 +1,81 @@
+import course_one from "../../../assets/images/homepage/course_1.jpg";
+import course_two from "../../../assets/images/homepage/course_2.jpg";
+import course_three from "../../../assets/images/homepage/course_3.jpg";
+import course_four from "../../../assets/images/homepage/course_4.jpg";
+import course_five from "../../../assets/images/homepage/course_5.jpg";
 const NewCareerRecommendations = () => {
+  const recomendedCourses = [
+    {
+      title: "The complete course of Bussines for beginners",
+      img: course_one,
+      instructor: "Adel nsiem",
+      price: 30,
+      review: 4.7,
+    },
+    {
+      title: "The complete course of Balence",
+      img: course_two,
+      instructor: "Adel nsiem",
+      price: 30,
+      review: 4.7,
+    },
+    {
+      title: "The complete course Swintific Rwriting",
+      img: course_three,
+      instructor: "Adel nsiem",
+      price: 30,
+      review: 4.7,
+    },
+    {
+      title: "The complete course of Machine Learing",
+      img: course_four,
+      instructor: "Adel nsiem",
+      price: 30,
+      review: 4.7,
+    },
+    {
+      title: "The complete course of Artfical for beginners",
+      img: course_five,
+      instructor: "Adel nsiem",
+      price: 30,
+      review: 4.7,
+    },
+    {
+      title: "The complete course of Bussines for beginners",
+      img: course_one,
+      instructor: "Adel nsiem",
+      price: 30,
+      review: 4.7,
+    },
+    {
+      title: "The complete course of Balence",
+      img: course_two,
+      instructor: "Adel nsiem",
+      price: 30,
+      review: 4.7,
+    },
+    {
+      title: "The complete course Swintific Rwriting",
+      img: course_three,
+      instructor: "Adel nsiem",
+      price: 30,
+      review: 4.7,
+    },
+    {
+      title: "The complete course of Machine Learing",
+      img: course_four,
+      instructor: "Adel nsiem",
+      price: 30,
+      review: 4.7,
+    },
+    {
+      title: "The complete course of Artfical for beginners",
+      img: course_five,
+      instructor: "Adel nsiem",
+      price: 30,
+      review: 4.7,
+    },
+  ];
   return <section>NewCareerRecommendations</section>;
 };
 
