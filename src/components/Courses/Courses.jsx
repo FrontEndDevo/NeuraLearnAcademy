93fe45c9d638664bd6b4acef3bd128e9283d9421
FrontEndDevo <mylinkacco@gmail.com>
2023-12-07 01:31:23 +0200

    Finish the second part of Courses component (AI programmer)

--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,3 +1,4 @@
+import programmer from "../../assets/images/homepage/programmer.jpg";
 const categories = [
   "all",
   "programming",
@@ -29,9 +30,30 @@ const Courses = () => {
         </p>
       </div>
 
-      <ul className="flex items-center justify-center gap-4">
+      <ul className="flex items-center justify-center gap-4 my-10">
         {coursesCategories}
       </ul>
+
+      <div className="relative p-8 bg-[#EEF3F6] border-2 shadow-lg rounded-3xl">
+        <img
+          src={programmer}
+          alt="programmer"
+          className="w-[250px] h-[250px] absolute -top-2 -left-20 border-8 border-primary-700 rounded-full"
+        />
+        <div className="flex items-center justify-center font-bold leading-7 text-justify px-[19rem] py-20">
+          <p>
+            Welcome, I am your assistant here to introduce you to
+            programming,software, and the world of software, and to create your
+            first application through a group of courses that qualify you for
+            the labor market with the latest existing technologies, knowing that
+            programming is fundamental to the technologies that humans have
+            achieved.
+          </p>
+        </div>
+        <button className="flex justify-end px-8 py-4 ml-auto text-2xl text-white capitalize duration-200 rounded-lg bg-primary-700 hover:bg-primary-500">
+          explore programming
+        </button>
+      </div>
     </section>
   );
 };
