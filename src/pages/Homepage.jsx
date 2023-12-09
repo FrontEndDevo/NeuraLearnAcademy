6b416adba1ca185f7fe236e5516652b1d00a10dd
FrontEndDevo <mylinkacco@gmail.com>
2023-12-10 00:22:19 +0200

    Reorder the components in Homepage.jsx

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,12 +2,18 @@ import Courses from "../components/Courses/Courses";
 import NewCareerRecommendations from "../components/NewCareerRecommendations/NewCareerRecommendations";
 import ProfessionalCertificates from "../components/ProfessionalCertificates/ProfessionalCertificates";
 import FaqComponent from "../components/FaqsQuestions/FaqComponent";
+import FeaturesSection from "../components/FeaturesSection/FeaturesSection";
+import ResourceGuide from "../components/ResourceGuide/ResourceGuide";
+import TopCategories from "../components/TopCategories/TopCategories";
 const Homepage = () => {
   return (
     <>
       <Courses />
-      <NewCareerRecommendations />
       <ProfessionalCertificates />
+      <ResourceGuide />
+      <NewCareerRecommendations />
+      <TopCategories />
+      <FeaturesSection />
       <FaqComponent />
     </>
   );
