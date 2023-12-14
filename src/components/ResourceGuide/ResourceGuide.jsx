9749da43a5f151a3f16b2c0f3b3ecdabbd4353ee
karim Abdelazim <ka1465@fayoum.edu.eg>
2023-12-15 00:17:20 +0200

    Edit ResourceGuide Componet and edit responsive it

--- src/components/ResourceGuide/ResourceGuide.jsx
+++ src/components/ResourceGuide/ResourceGuide.jsx
@@ -2,35 +2,38 @@ import Image1 from "../../assets/images/homepage/ResourceGuideSection/men.jpg";
 function ResourcesGuide() {
   return (
     <>
-      <div className="  h-screen  p-28 w bg-white  ">
-        <div className="grid sm:grid-cols-1  md:grid-cols-1 lg:grid-cols-2   ">
+      <div className="  h-screen p-8 md:p-14 lg:p-16  xl:p-28   bg-white  ">
+        <div className="grid  xl:grid-cols-2 gap-8 md:gap-8 xl:gap-28    ">
           <div className="ResourceGuideImage ">
-            <img src={Image1} className="" width="550px" alt="" />
+            <img src={Image1} alt="" />
           </div>
 
           {/* Content  */}
           <div className="ResorceGuideContent">
-            <p className=" mb-2 text-sm">
+            <p className=" mb-2 text-sm md:text-1xl ">
               Finished a Professional certificate?
             </p>
-            <h2 className=" mb-10 text-2xl text-gray-700">
+            <h2 className=" mb-16  md:text-2xl   text-gray-700">
               Get resources and support to guide you
               <br /> through the jop search process.
             </h2>
-            <h3 className=" text-2xl	">showcase your skills</h3>
-            <p className="mt-2 mb-4 text-sm">
+            <h3 className=" md:text-2xl font-medium	">showcase your skills</h3>
+            <p className="mt-3 mb-8 text-sm">
               present real world projects to potentail employers.
             </p>
-            <h3 className=" text-2xl	">Enhance your resume</h3>
-            <p className="mt-2 mb-4 text-sm">
+            <h3 className=" md:text-2xl font-medium		">Enhance your resume</h3>
+            <p className="mt-3 mb-8 text-sm">
               Get support to build a standout resume and LinkedIn profile
             </p>
-            <h3 className=" text-2xl	">Ace your interview</h3>
-            <p className="mt-2 mb-4 text-sm">
+            <h3 className=" md:text-2xl font-medium		">Ace your interview</h3>
+            <p className="mt-3 mb-8  text-sm">
               Practice and improve your interview skills with virtual feedback.
             </p>
           </div>
+
+
         </div>
+
       </div>
     </>
   );
