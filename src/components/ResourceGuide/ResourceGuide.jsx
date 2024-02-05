2c6996432ed8d4438e913b37303f99b6cd445e26
karim Abdelazim <ka1465@fayoum.edu.eg>
2024-02-05 15:07:11 +0200

    add last update Resource GuideSection

--- src/components/ResourceGuide/ResourceGuide.jsx
+++ src/components/ResourceGuide/ResourceGuide.jsx
@@ -2,38 +2,34 @@ import Image1 from "../../assets/images/homepage/ResourceGuideSection/men.jpg";
 function ResourcesGuide() {
   return (
     <>
-      <div className="  container my-10 lg:my-20 p-8 md:p-10 lg:p-16 xl:p-0  bg-white  ">
-          <div className="grid  xl:grid-cols-2 gap-8 md:gap-8 xl:gap-28    ">
-            <div className="ResourceGuideImage ">
-              <img src={Image1}  alt="" />
-            </div>
+      <div className="container my-10 xl:my-32 p-8  lg:p-16 xl:p-0 bg-white">
+        <div className="grid grid-cols-1 xl:grid-cols-2 gap-8 md:gap-4 xl:gap-28">
+          <div className="flex justify-center items-center md:w-3/4 lg:w-10/12 xl:w-full">
+            <img src={Image1} alt="" />
+          </div>
 
-            {/* Content  */}
-            <div className="ResorceGuideContent">
-              <p className=" mb-2 text-sm md:text-1xl ">
-                Finished a Professional certificate?
-              </p>
-            <h2 className=" mb-16  md:text-2xl   text-gray-700">
-                Get resources and support to guide you
-                <br /> through the jop search process.
-              </h2>
-              <h3 className=" md:text-2xl font-medium	">showcase your skills</h3>
-              <p className="mt-3 mb-8 text-sm">
-                present real world projects to potentail employers.
-              </p>
-            <h3 className=" md:text-2xl font-medium		">Enhance your resume</h3>
-              <p className="mt-3 mb-8 text-sm">
-                Get support to build a standout resume and LinkedIn profile
-              </p>
-            <h3 className=" md:text-2xl font-medium		">Ace your interview</h3>
-              <p className="mt-3 mb-8  text-sm">
-                Practice and improve your interview skills with virtual feedback.
-              </p>
-            </div>
-        
-
+          {/* Content */}
+          <div className="">
+            <p className="mb-2 text-sm md:text-base lg:text-lg font-bold text-blue-700">
+              Finished a Professional certificate?
+            </p>
+            <h2 className="mb-6 xl:mb-16 text-lg md:text-sm lg:text-2xl text-gray-700">
+              Get resources and support to guide you through the job search process.
+            </h2>
+            <h3 className="md:text-lg lg:text-xl font-semibold">Showcase your skills</h3>
+            <p className="mt-3 mb-6 text-sm md:text-base lg:text-lg">
+              Present real-world projects to potential employers.
+            </p>
+            <h3 className="md:text-lg lg:text-xl font-semibold">Enhance your resume</h3>
+            <p className="mt-3 mb-6 text-sm md:text-base lg:text-lg">
+              Get support to build a standout resume and LinkedIn profile.
+            </p>
+            <h3 className="md:text-lg lg:text-xl font-semibold">Ace your interview</h3>
+            <p className="mt-3 mb-6 text-sm md:text-base lg:text-lg">
+              Practice and improve your interview skills with virtual feedback.
+            </p>
+          </div>
         </div>
-
       </div>
     </>
   );
