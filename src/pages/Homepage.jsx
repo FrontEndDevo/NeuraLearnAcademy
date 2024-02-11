f1218e144c4a57d6c682c0729cf55608e18db3e7
FrontEndDevo <mylinkacco@gmail.com>
2024-02-11 22:18:52 +0200

    Delete ProfessionalCertificates component as We created another one with different form

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,5 @@
 import Courses from "../components/Courses/Courses";
 import NewCareerRecommendations from "../components/NewCareerRecommendations/NewCareerRecommendations";
-import ProfessionalCertificates from "../components/ProfessionalCertificates/ProfessionalCertificates";
 import FaqComponent from "../components/FaqsQuestions/FaqComponent";
 import FeaturesSection from "../components/FeaturesSection/FeaturesSection";
 import ResourceGuide from "../components/ResourceGuide/ResourceGuide";
@@ -11,11 +10,10 @@ const Homepage = () => {
     <>
       <Hero />
       <Courses />
-      <ProfessionalCertificates />
+      <FeaturesSection />
       <ResourceGuide />
       <NewCareerRecommendations />
       <TopCategories />
-      <FeaturesSection />
       <FaqComponent />
     </>
   );
