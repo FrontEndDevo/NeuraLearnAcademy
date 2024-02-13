d5dbb7c21a5fd7316b0df09de3e03d9f606109d7
FrontEndDevo <mylinkacco@gmail.com>
2024-02-13 16:29:54 +0200

    Add Repsonsive styles to Specializations component and add the required classes to index.css file

--- src/components/Specializations/Specializations.jsx
+++ src/components/Specializations/Specializations.jsx
@@ -39,12 +39,16 @@ const Specializations = () => {
   const renderedOurSpecializations = (
     <Slider {...settings}>
       {ourSpecializations.map((item, index) => (
-        <div key={index} className="bg-gray-300 rounded-2xl">
-          <div className="grid items-center grid-cols-2 gap-8">
-            <img src={item.image} alt={item.title} className="w-full h-52" />
-            <div className="pr-6">
-              <h2 className="text-xl font-bold">{item.title}</h2>
-              <p className="text-sm font-semibold leading-6">
+        <div key={index} className="bg-zinc-300 rounded-2xl">
+          <div className="grid items-center grid-cols-2 gap-4 md:gap-8">
+            <img
+              src={item.image}
+              alt={item.title}
+              className="w-full h-full rounded-l-2xl"
+            />
+            <div className="pr-4 md:pr-6">
+              <h2 className="text-base font-bold md:text-xl">{item.title}</h2>
+              <p className="text-xs font-semibold leading-snug text-justify md:leading-6 md:text-sm">
                 {item.description}
               </p>
             </div>
@@ -55,25 +59,25 @@ const Specializations = () => {
   );
 
   return (
-    <section className="grid grid-cols-2">
-      <div className="pb-40 pl-20 pr-10 pt-80 left-specializations-linear-gradient">
-        <h1 className="text-[#FFCD3F] font-bold text-4xl">
+    <section className="grid-cols-2 md:grid">
+      <div className="pt-40 pb-10 pl-10 pr-6 lg:pr-10 md:pb-40 lg:pl-20 md:pt-80 left-specializations-linear-gradient">
+        <h1 className="text-[#FFCD3F] font-bold text-xl md:text-3xl lg:text-4xl leading-loose md:leading-[50px] lg:leading-[50px]">
           Master and gain essential skills with Neura Specializations
         </h1>
-        <p className="my-16 text-base text-white">
+        <p className="py-4 mb-10 text-xs leading-snug text-justify text-white md:my-10 md:text-sm lg:my-16 lg:text-base">
           Develop a specific career skill through a series of related courses
           and hands-on projects. Put theory into practice and earn a
           Specialization Certificate to add to your CV
         </p>
         <Link
           to="/"
-          className="px-8 py-4 bg-white rounded-full text-primary-500"
+          className="px-4 py-2 text-xs font-bold duration-300 bg-white rounded-full md:text-base md:px-8 md:py-4 text-primary-500 hover:text-primary-700"
         >
           Explore specializations with us
         </Link>
       </div>
 
-      <div className="pb-40 pl-20 pr-10 specializations right-specializations-linear-gradient pt-80">
+      <div className="px-4 py-10 md:pl-20 md:pr-10 md:pb-40 specializations right-specializations-linear-gradient md:pt-80">
         {renderedOurSpecializations}
       </div>
     </section>
