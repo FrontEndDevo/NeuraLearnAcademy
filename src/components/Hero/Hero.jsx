69b57a8002061ba7370f7a83d321db611c85a299
BasemYahia402 <basemyahia70@gmail.com>
2024-02-13 21:32:08 +0200

    update benefits section with icons and text.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -4,10 +4,37 @@ import { faHandPointer } from "@fortawesome/free-solid-svg-icons";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import hero from "/src/assets/images/homepage/hero.png";
 import { Link } from "react-router-dom";
+const benifitsArray = [
+  {
+    icon: faArrowRight,
+    title: "Build",
+    content: "Take the first step toward your new career",
+  },
+  {
+    icon: faCertificate,
+    title: "Discover",
+    content: "a wide variety of quality courses and specializations.",
+  },
+  {
+    icon: faHandPointer,
+    title: "Enroll",
+    content: "join in with peers eager to learn–just like you.",
+  },
+  {
+    icon: faCertificate,
+    title: "Learn",
+    content: "with world-class instructors and hone your professional.",
+  },
+  {
+    icon: faCertificate,
+    title: "Get Certified",
+    content: "and boost your chances at launching or advancing your career.",
+  },
+];
 const Hero = () => {
   return (
-    <div className="mx-4 mt-24 md:mx-12">
-      <section className="flex items-center justify-center">
+    <div className="mx-4 mt-24 md:mx-8 lg:mx-12">
+      <section className="grid items-center justify-center">
         <div>
           <div className="p-4 mb-10 border-t-2 border-solid shadow-xl lg:w-11/12 lg:p-8">
             <h1 className="mb-4 text-2xl font-bold">Learn. Improve. Grow.</h1>
@@ -27,58 +54,19 @@ const Hero = () => {
           <img src={hero} alt="" />
         </div>
       </section>
-
-      <section className="grid w-full grid-cols-1 gap-5 p-3 mx-auto mt-8 text-center border shadow-md rounded-xl md:p-8 md:w-11/12 md:grid-cols-2 lg:grid-cols-5 md:text-left">
-        <div className="md:col-start-1 md:col-end-3 lg:col-end-2">
-          <h2 className="text-xl font-bold">
-            Take the first step toward your new career
-          </h2>
-          <FontAwesomeIcon
-            icon={faArrowRight}
-            className="mt-5 text-red-700"
-            size="3x"
-          />
-        </div>
-        <div>
-          <FontAwesomeIcon
-            icon={faCertificate}
-            className="mb-6 text-teal-500"
-            size="3x"
-          />
-          <h2 className="text-lg font-bold">Discover</h2>
-          <p>a wide variety of quality courses and specializations. </p>
-        </div>
-
-        <div>
-          <FontAwesomeIcon
-            icon={faHandPointer}
-            className="mb-6 text-teal-500"
-            size="3x"
-          />
-          <h2 className="text-lg font-bold">Enroll</h2>
-          <p> join in with peers eager to learn–just like you. </p>
-        </div>
-
-        <div>
-          <FontAwesomeIcon
-            icon={faCertificate}
-            className="mb-6 text-teal-500"
-            size="3x"
-          />
-          <h2 className="text-lg font-bold">Learn</h2>
-          <p>with world-class instructors and hone your professional </p>
-        </div>
-
-        <div>
-          <FontAwesomeIcon
-            icon={faCertificate}
-            className="mb-6 text-teal-500"
-            size="3x"
-          />
-          <h2 className="text-lg font-bold">Get Certified</h2>
-          <p>and boost your chances at launching or advancing your career.</p>
-        </div>
-      </section>
+      <ul className="relative z-0 grid w-full grid-cols-1 gap-5 p-3 mx-auto mt-8 text-center bg-white border shadow-md -mb-28 rounded-xl md:p-8 md:w-11/12 md:grid-cols-2 lg:grid-cols-5 md:text-left">
+        {benifitsArray.map((item, index) => (
+          <li key={index}>
+            <FontAwesomeIcon
+              icon={item.icon}
+              className="mb-6 text-teal-500"
+              size="3x"
+            />
+            <h2 className="text-lg font-bold">{item.title}</h2>
+            <p>{item.content} </p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
