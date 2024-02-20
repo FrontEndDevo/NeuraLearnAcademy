bb4851b2ac263c7ec4bd0587dec72060427a06a7
karim Abdelazim <ka1465@fayoum.edu.eg>
2024-02-20 23:34:24 +0200

    change name of folder and file

--- src/components/ShopingCart/Cart.jsx
+++ src/components/ShopingCart/Cart.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Cart() {
+function ShopingCart() {
     return (
         <>
 
@@ -9,4 +9,4 @@ function Cart() {
     )
 }
 
-export default Cart
+export default ShopingCart;
