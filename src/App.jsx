35ab2580b6e9fd4a561a8bfd6bd2868cbe21b8c2
karim Abdelazim <ka1465@fayoum.edu.eg>
2024-02-20 23:45:58 +0200

    making routing to cart

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Homepage from "./pages/Homepage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import MyLearningsPage from "./pages/MyLearningsPage";
+import ShopingCart from "./components/ShopingCart/ShopingCart";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/my-learnings" element={<MyLearningsPage />} />
+        <Route path="/cart" element={<ShopingCart />} />
       </Routes>
     </>
   );
