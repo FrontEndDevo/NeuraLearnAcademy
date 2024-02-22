fb7b81a08b32073ec48cdba651cf63642e8d0ab7
karim Abdelazim <ka1465@fayoum.edu.eg>
2024-02-22 03:11:33 +0200

    edit the place of routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Homepage from "./pages/Homepage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import MyLearningsPage from "./pages/MyLearningsPage";
+
+
 import ShoppingCartPage from "./pages/ShoppingCartPage";
 
 const App = () => {
@@ -13,6 +15,8 @@ const App = () => {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/my-learnings" element={<MyLearningsPage />} />
+
+
         <Route path="/cart" element={<ShoppingCartPage />} />
       </Routes>
     </>
