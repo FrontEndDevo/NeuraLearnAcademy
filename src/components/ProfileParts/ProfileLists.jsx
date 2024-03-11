d89bb41382d04b2307127850dd61d8d922133116
FrontEndDevo <mylinkacco@gmail.com>
2024-03-11 18:55:13 +0200

    Render the main JSX structure

--- src/components/ProfileParts/ProfileLists.jsx
+++ src/components/ProfileParts/ProfileLists.jsx
@@ -1,6 +1,38 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import userPhoto from "../../assets/images/LoginSigin/logo.png";
+import { faCamera } from "@fortawesome/free-solid-svg-icons";
+const userInfo = {
+  name: "Ahmed Ashraf",
+  photo: userPhoto,
+};
+
+const profileSections = [
+  "profile",
+  "password & security",
+  "privacy",
+  "notification",
+  "close account",
+];
 const ProfileLists = () => {
-  return <aside></aside>;
+  // render the profile sections:
+  const userProfileSections = profileSections.map((section, index) => (
+    <li key={index}>{section}</li>
+  ));
+
+  return (
+    <aside>
+      <div>
+        <img src={userInfo.photo} alt={userInfo.name} />
+        <div>
+          <input type="file" name="photo" id="Photo" />
+          <FontAwesomeIcon icon={faCamera} />
+        </div>
+        <h1>{userInfo.name}</h1>
+      </div>
+
+      <ul>{userProfileSections}</ul>
+    </aside>
+  );
 };
 
 export default ProfileLists;
