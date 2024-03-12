9bcf560aed8836239a7fc0902661ef04a685b340
FrontEndDevo <mylinkacco@gmail.com>
2024-03-12 02:04:33 +0200

    Add user profile photo upload functionality to update user photo when upload a new one

--- src/components/ProfileParts/ProfileLists.jsx
+++ src/components/ProfileParts/ProfileLists.jsx
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import userPhoto from "../../assets/images/profile/unknown_user.webp";
 import { faCamera } from "@fortawesome/free-solid-svg-icons";
+import { useRef, useState } from "react";
 
 const userInfo = {
   name: "Ahmed Ashraf",
@@ -15,6 +16,26 @@ const profileSections = [
   "close account",
 ];
 const ProfileLists = () => {
+  const fileInput = useRef(null);
+  const [userPhoto, setUserPhoto] = useState(userInfo.photo); // initial image (Unknown user)
+
+  // handle the icon click to open the file input
+  const handleIconClick = () => {
+    // trigger the file input
+    fileInput.current.click();
+  };
+
+  // handle the file change to update the photo of the user profile
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    // update the photo
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setUserPhoto(reader.result);
+    };
+    reader.readAsDataURL(file);
+  };
+
   // render the profile sections:
   const userProfileSections = profileSections.map((section, index) => (
     <li key={index}>{section}</li>
@@ -22,11 +43,22 @@ const ProfileLists = () => {
 
   return (
     <aside>
-      <div>
-        <img src={userInfo.photo} alt={userInfo.name} />
-        <div>
-          <input type="file" name="photo" id="Photo" />
-          <FontAwesomeIcon icon={faCamera} />
+      <div className="relative">
+        <img
+          src={userPhoto}
+          alt={userInfo.name}
+          className="w-40 h-40 rounded-full"
+        />
+        <div className="absolute">
+          <input
+            type="file"
+            name="photo"
+            id="Photo"
+            ref={fileInput}
+            className="hidden"
+            onChange={handleFileChange}
+          />
+          <FontAwesomeIcon icon={faCamera} onClick={handleIconClick} />
         </div>
         <h1>{userInfo.name}</h1>
       </div>
