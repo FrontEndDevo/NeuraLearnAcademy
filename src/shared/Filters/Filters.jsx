35ba2d075083c8422d9cef2bc60e3ffb63cb6736
FrontEndDevo <mylinkacco@gmail.com>
2024-03-14 23:53:24 +0200

    Create Filters folder and update import path for Filters component

--- src/shared/Filters/Filters.jsx
+++ src/shared/Filters/Filters.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { faAngleDown, faSliders } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { sortFilterClasses } from "../components/UserCourses/UserCoursesOptions/UserCoursesActions/SortUserCourses";
+import { sortFilterClasses } from "../../components/UserCourses/UserCoursesOptions/UserCoursesActions/SortUserCourses";
 
 const Filters = React.memo(() => {
   return (
