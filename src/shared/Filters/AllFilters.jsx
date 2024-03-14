820e33f920bfd33db5ac8ddc10e1f8197556b832
FrontEndDevo <mylinkacco@gmail.com>
2024-03-15 00:32:57 +0200

    Add filter toggle functionality and display filters when filters are open

--- src/shared/Filters/AllFilters.jsx
+++ src/shared/Filters/AllFilters.jsx
@@ -1,18 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { faAngleDown, faSliders } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Topics from "./Topics";
 
 const AllFilters = React.memo(() => {
+  const [areFiltersOpen, setAreFiltersOpen] = useState(true);
   return (
-    <div className="items-center justify-between py-3 md:py-4 px-2 rounded-[5px] cursor-pointer border-b-2 border-zinc-600 duration-200 hover:bg-zinc-700 hover:text-white flex">
-      <div className="flex items-center gap-2">
-        <FontAwesomeIcon icon={faSliders} />
-        <h4 className="text-xs font-bold tracking-wide lg:text-sm xl:text-base">
-          Filters
-        </h4>
+    <>
+      <div
+        onClick={() => setAreFiltersOpen((prevState) => !prevState)}
+        className="items-center justify-between py-3 md:py-4 px-2 rounded-[5px] cursor-pointer border-b-2 border-zinc-600 duration-200 hover:bg-zinc-700 hover:text-white flex"
+      >
+        <div className="flex items-center gap-2">
+          <FontAwesomeIcon icon={faSliders} />
+          <h4 className="text-xs font-bold tracking-wide lg:text-sm xl:text-base">
+            Filters
+          </h4>
+        </div>
+        <FontAwesomeIcon
+          icon={faAngleDown}
+          className={`duration-200 ${
+            areFiltersOpen ? "rotate-180" : "rotate-0"
+          }`}
+        />
       </div>
-      <FontAwesomeIcon icon={faAngleDown} />
-    </div>
+      <div className={`${areFiltersOpen ? "block" : "hidden"}`}>
+        <Topics />
+      </div>
+    </>
   );
 });
 
