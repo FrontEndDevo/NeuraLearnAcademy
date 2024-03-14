fec15727680bec91be18bd8c3114a63eb0713e33
FrontEndDevo <mylinkacco@gmail.com>
2024-03-14 18:38:45 +0200

    Finish the categories of the aside section with styles

--- src/components/AllCourses/CoursesCategories.jsx
+++ src/components/AllCourses/CoursesCategories.jsx
@@ -1,5 +1,56 @@
+import { Link } from "react-router-dom";
+import Slider from "react-slick/lib/slider";
+
+const allCategories = [
+  "all",
+  "programming",
+  "development",
+  "data science",
+  "business",
+  "marketing",
+  "sales",
+  "drawing",
+  "photography",
+  "category_1",
+  "category_2",
+  "category_3",
+  "category_4",
+];
+// Slider settings.
+const settings = {
+  dots: false,
+  arrows: false,
+  infinite: true,
+  slidesToShow: 9,
+  slidesToScroll: 1,
+  vertical: true,
+  verticalSwiping: true,
+  swipeToSlide: true,
+  autoplay: true,
+  speed: 1000,
+  autoplaySpeed: 3000,
+  cssEase: "ease-in-out",
+};
 const CoursesCategories = () => {
-  return <aside>CoursesCategories</aside>;
+  // Render the courses categories first.
+  const renderedCoursesCategories = allCategories.map((cat, i) => (
+    <Link
+      key={i}
+      to={cat.replace(/[^a-zA-Z0-9 ]/g, "").replace(/\s+/g, "-")}
+      className={`px-4 py-2 -my-10 duration-200 hover:bg-primary-500 hover:text-white capitalize text-lg font-semibold ${
+        cat.toLocaleLowerCase() == "all" ? "bg-primary-500 text-white" : ""
+      }`}
+    >
+      {cat}
+    </Link>
+  ));
+  return (
+    <aside>
+      <div className="border-b-2 border-gray-700">
+        <Slider {...settings}>{renderedCoursesCategories}</Slider>
+      </div>
+    </aside>
+  );
 };
 
 export default CoursesCategories;
--- src/pages/AllCoursesPage.jsx
+++ src/pages/AllCoursesPage.jsx
@@ -2,13 +2,13 @@ import CoursesCategories from "../components/AllCourses/CoursesCategories";
 
 const AllCoursesPage = () => {
   return (
-    <section className="container grid grid-cols-4 gap-2">
+    <section className="grid grid-cols-6 gap-2 m-10">
       <div className="col-span-1">
         {/* Aside + Search + Sort + Filters */}
         <CoursesCategories />
       </div>
 
-      <div className="col-span-3">
+      <div className="col-span-5">
         <h1>courses</h1>
       </div>
     </section>
