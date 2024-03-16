badc416c3c636555ed72702500db501b64f95c48
FrontEndDevo <mylinkacco@gmail.com>
2024-03-17 00:38:13 +0200

    Remove commented code for Aside + Search + Sort + Filters in AllCoursesPage.jsx

--- src/pages/AllCoursesPage.jsx
+++ src/pages/AllCoursesPage.jsx
@@ -21,7 +21,6 @@ const AllCoursesPage = () => {
   return (
     <section className="grid grid-cols-6 gap-2 m-10">
       <div className="col-span-1">
-        {/* Aside + Search + Sort + Filters */}
         <CoursesCategories categories={allCategories} />
       </div>
 
