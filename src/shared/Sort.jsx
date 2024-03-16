c9e6bcf7539c86047524d0ebd029827f701cf60b
FrontEndDevo <mylinkacco@gmail.com>
2024-03-16 02:08:34 +0200

    Add sort logic to Sort component

--- src/shared/Sort.jsx
+++ src/shared/Sort.jsx
@@ -1,18 +1,57 @@
 import { faAngleDown, faArrowDownAZ } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
 
-const Sort = () => {
+const sortOptions = ["most viewed", "highest related", "newest"];
+
+const Sort = React.memo(() => {
+  const [isSortOpen, setIsSortOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const chooseSortOption = (option) => {
+    setSelectedOption(option);
+  };
+
+  const sortDropdown = sortOptions.map((opt, i) => (
+    <li
+      key={i}
+      onClick={() => chooseSortOption(opt)}
+      className="px-3 py-2 text-sm font-semibold capitalize duration-200 cursor-pointer hover:bg-indigo-500 hover:text-white"
+    >
+      {opt}
+    </li>
+  ));
+
   return (
-    <div className="items-center justify-between py-3 md:py-4 px-2 rounded-[5px] cursor-pointer border-b-2 border-zinc-600 duration-200 hover:bg-zinc-700 hover:text-white flex">
-      <div className="flex items-center gap-2">
-        <FontAwesomeIcon icon={faArrowDownAZ} />
-        <h4 className="text-xs font-bold tracking-wide lg:text-sm xl:text-base">
-          Sort
-        </h4>
+    <div className="relative">
+      <div
+        onClick={() => setIsSortOpen((prevState) => !prevState)}
+        className="items-center justify-between py-3 md:py-4 px-2 rounded-[5px] cursor-pointer border-b-2 border-zinc-600 duration-200 hover:bg-zinc-700 hover:text-white flex"
+      >
+        <div className="flex items-center gap-2">
+          {!selectedOption && <FontAwesomeIcon icon={faArrowDownAZ} />}
+          <h4 className="text-xs font-bold tracking-wide lg:text-sm xl:text-base">
+            {selectedOption ? selectedOption : "Sort by"}
+          </h4>
+        </div>
+        <FontAwesomeIcon
+          icon={faAngleDown}
+          className={`duration-200 ${isSortOpen ? "rotate-180" : "rotate-0"}`}
+        />
       </div>
-      <FontAwesomeIcon icon={faAngleDown} />
+      <ul
+        className={`absolute z-50 duration-200 w-full p-2 bg-white border shadow-lg rounded-md ${
+          isSortOpen
+            ? "opacity-100 translate-y-0"
+            : "md:opacity-0 md:-translate-y-5"
+        }`}
+      >
+        {sortDropdown}
+      </ul>
     </div>
   );
-};
+});
+
+Sort.displayName = "Sort";
 
 export default Sort;
