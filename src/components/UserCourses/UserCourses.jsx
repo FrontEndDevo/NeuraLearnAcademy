5d96650bd3ffacaa7f07ed8a705073d57e32714d
FrontEndDevo <mylinkacco@gmail.com>
2024-03-17 00:42:30 +0200

    Exchange the course logic with CourseCard component to improve performance and readability

--- src/components/UserCourses/UserCourses.jsx
+++ src/components/UserCourses/UserCourses.jsx
@@ -3,308 +3,308 @@ import UserCoursesOptions from "./UserCoursesOptions";
 import course1 from "../../assets/images/homepage/course_9.jpg";
 import course2 from "../../assets/images/homepage/course_8.jpg";
 import course3 from "../../assets/images/homepage/course_7.jpg";
-import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Pagination from "../../shared/Pagination";
+import CourseCard from "../../shared/Courses/CourseCard";
 const userCourses = ["courses", "my lists", "wishlist", "archived", "purchase"];
 const allUserCourses = [
   {
     image: course1,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming object for beginners",
     category: "Programming",
     progress: 0,
   },
   {
     image: course2,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 10,
   },
   {
     image: course3,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 30,
   },
   {
     image: course1,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 50,
   },
   {
     image: course2,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 80,
   },
   {
     image: course3,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 100,
   },
   {
     image: course1,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 50,
   },
   {
     image: course2,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 80,
   },
   {
     image: course3,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of object oriented programming for beginners",
     category: "Programming",
     progress: 100,
   },
   {
     image: course3,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 100,
   },
   {
     image: course1,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 50,
   },
   {
     image: course2,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 80,
   },
   {
     image: course3,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 100,
   },
   {
     image: course1,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 50,
   },
   {
     image: course2,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 80,
   },
   {
     image: course3,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 100,
   },
   {
     image: course1,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 50,
   },
   {
     image: course2,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 80,
   },
   {
     image: course3,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of object oriented programming for beginners",
     category: "Programming",
     progress: 100,
   },
   {
     image: course3,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 100,
   },
   {
     image: course1,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 50,
   },
   {
     image: course2,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 80,
   },
   {
     image: course3,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 100,
   },
   {
     image: course1,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 50,
   },
   {
     image: course2,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 80,
   },
   {
     image: course3,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 100,
   },
   {
     image: course1,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 50,
   },
   {
     image: course2,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 80,
   },
   {
     image: course3,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of object oriented programming for beginners",
     category: "Programming",
     progress: 100,
   },
   {
     image: course3,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 100,
   },
   {
     image: course1,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 50,
   },
   {
     image: course2,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 80,
   },
   {
     image: course3,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 100,
   },
   {
     image: course1,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 50,
   },
   {
     image: course2,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 80,
   },
   {
     image: course3,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 100,
   },
   {
     image: course1,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 50,
   },
   {
     image: course2,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 80,
   },
   {
     image: course3,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of object oriented programming for beginners",
     category: "Programming",
     progress: 100,
   },
   {
     image: course3,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 100,
   },
   {
     image: course1,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 50,
   },
   {
     image: course2,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 80,
   },
   {
     image: course3,
-    author: "Adel nsiem",
+    instructor: "Adel nsiem",
     title: "The complete course of programming for beginners",
     category: "Programming",
     progress: 100,
@@ -351,72 +351,16 @@ const UserCourses = () => {
   const correctCoursesArray = searchKeyword ? filteredCourses : allUserCourses;
   const renderedUserCourses = correctCoursesArray
     .slice(paginationIndices.start, paginationIndices.end)
-    .map((course, index) => {
-      // Classes:
-      const progressBarClasses = "absolute top-0 left-0 h-1 rounded-full";
-      const threeDotsClasses = "w-2 h-2 rounded-full bg-gray-color-500";
-
-      const courseTitle =
-        course.title.length <= 50
-          ? course.title
-          : course.title.slice(0, 50) + "...";
-
-      // User progress button:
-      const progressButton =
-        course.progress == 0
-          ? "Start Learning"
-          : course.progress == 100
-          ? "Review"
-          : "Continue Learning";
-      return (
-        <li key={index}>
-          <div className="border shadow-lg h-[34rem] md:h-[36rem] lg:h-[44rem] xl:h-[40rem] 2xl:h-[38rem] rounded-3xl">
-            <img
-              src={course.image}
-              alt={course.title}
-              className="w-full h-1/2 rounded-t-3xl"
-            />
-            <div className="p-4">
-              <div className="flex items-center justify-between px-2">
-                <span className="text-xs font-bold text-gray-color-500 md:text-sm">
-                  {course.author}
-                </span>
-                <div className="flex gap-1 p-2 cursor-pointer lg:hover:animate-pulse">
-                  {Array.from({ length: 3 }, (_, i) => (
-                    <span key={i} className={threeDotsClasses} />
-                  ))}
-                </div>
-              </div>
-
-              <h3 className="my-4 text-base font-semibold leading-6 tracking-wide lg:text-xl text-gray-color-700">
-                {courseTitle}
-              </h3>
-              <h6 className="px-3 py-1 text-[10px] lg:text-base font-bold text-white rounded-full bg-secondary-700 w-fit">
-                {course.category}
-              </h6>
-              <div className="relative my-4">
-                <span
-                  style={{ width: `${course.progress}%` }}
-                  className={`${progressBarClasses} duration-500 bg-primary-500`}
-                />
-                <span
-                  className={`${progressBarClasses} w-full bg-[#82818166] -z-10`}
-                />
-                <p className="text-[9px] pt-3 md:text-sm font-semibold">
-                  {course.progress}% complete
-                </p>
-              </div>
-              <Link
-                to="/"
-                className="flex items-center justify-center px-12 py-2 text-xs text-white duration-300 rounded-full md:px-8 lg:px-10 lg:py-3 md:text-sm bg-primary-500 hover:bg-primary-700"
-              >
-                {progressButton}
-              </Link>
-            </div>
-          </div>
-        </li>
-      );
-    });
+    .map((course, index) => (
+      <CourseCard
+        key={index}
+        image={course.image}
+        title={course.title}
+        instructor={course.instructor}
+        category={course.category}
+        progress={course.progress}
+      />
+    ));
 
   return (
     <section className="container grid grid-cols-1 px-2 py-10 gap-y-10 md:gap-x-8 md:grid-cols-3 lg:gap-10 lg:grid-cols-4">
