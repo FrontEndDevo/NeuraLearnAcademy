bf0b8e0adeb0532f666fa2ece6c307bdc07b0d34
FrontEndDevo <mylinkacco@gmail.com>
2024-03-19 15:00:53 +0200

    Refactor topicSlice actions to use toggleTopic

--- src/redux/slices/Filters/topics.js
+++ src/redux/slices/Filters/topics.js
@@ -4,15 +4,19 @@ const topicSlice = createSlice({
   name: "topics",
   initialState: [],
   reducers: {
-    addTopic: (state, action) => {
-      state.push(action.payload);
-    },
-    removeTopic: (state, action) => {
-      return state.filter((topic) => topic !== action.payload);
+    toggleTopic: (state, action) => {
+      const index = state.indexOf(action.payload);
+      if (index === -1) {
+        // The topic is not in the array, so add it.
+        state.push(action.payload);
+      } else {
+        // The topic is in the array, so remove it.
+        state.splice(index, 1);
+      }
     },
     resetTopics: () => [],
   },
 });
 
-export const { addTopic, removeTopic, resetTopics } = topicSlice.actions;
+export const { toggleTopic, resetTopics } = topicSlice.actions;
 export default topicSlice.reducer;
