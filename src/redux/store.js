3b353985b1c9ecaf9e32cb5d2c250633b7edb60e
FrontEndDevo <mylinkacco@gmail.com>
2024-03-19 15:26:28 +0200

    Subscribe filter slices to the Redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import searchCoursesReducer from "./slices/searchCourses";
+import ratingsSlice from "./slices/Filters/ratings";
+import priceSlice from "./slices/Filters/prices";
+import topicSlice from "./slices/Filters/topics";
+
 export const store = configureStore({
   reducer: {
     searchCourses: searchCoursesReducer,
+    ratings: ratingsSlice,
+    prices: priceSlice,
+    topics: topicSlice,
   },
 });
