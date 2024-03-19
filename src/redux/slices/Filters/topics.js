9d1dbcb55244d02f07baa0cc1f44b596ad28c3f0
FrontEndDevo <mylinkacco@gmail.com>
2024-03-19 14:43:31 +0200

    Refactor topics slice in redux to accept array of strings not just a single string

--- src/redux/slices/Filters/topics.js
+++ src/redux/slices/Filters/topics.js
@@ -1,13 +1,18 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const topicSlice = createSlice({
-  name: "topic",
-  initialState: "",
+  name: "topics",
+  initialState: [],
   reducers: {
-    setTopic: (state, action) => action.payload,
-    resetTopic: () => "",
+    addTopic: (state, action) => {
+      state.push(action.payload);
+    },
+    removeTopic: (state, action) => {
+      return state.filter((topic) => topic !== action.payload);
+    },
+    resetTopics: () => [],
   },
 });
 
-export const { setTopic, resetTopic } = topicSlice.actions;
+export const { addTopic, removeTopic, resetTopics } = topicSlice.actions;
 export default topicSlice.reducer;
