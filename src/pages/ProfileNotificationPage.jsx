cca9d19540b9ed020bfc82a6df62de4b3aa66d4c
BasemYahia402 <basemyahia70@gmail.com>
2024-03-24 01:16:58 +0200

    Add notification settings to ProfileNotificationPage

--- src/pages/ProfileNotificationPage.jsx
+++ src/pages/ProfileNotificationPage.jsx
@@ -1,9 +1,27 @@
+import ProfileHeader from "../shared/ProfileHeader";
+/* eslint-disable */
 const ProfileNotificationPage = () => {
-    return (
-        <div>
-            <h1 className="text-center">Profile Notification</h1>
-        </div>
-    );
-}
+  const title = "Notifications";
+  const description =
+    "Turn promotional email notifications from Udemy on or off";
+  return (
+    <section className="py-8 border border-b-0 border-gray-400 ">
+      <ProfileHeader title={title} description={description} />
+      <div className="px-3 py-8 font-bold leading-8 md:px-5 lg:px-10 md:text-xl">
+        <h3 className="mb-4">I want to receive:</h3>
+        <p className="mb-4">
+          Promotions, course recommendations, and helpful resources.
+        </p>
+        <p className="mb-4">
+          Announcements from instructors whose course(s) I’m enrolled in.
+        </p>
+        <p className="mb-4">Don't send me any promotional emails.</p>
+        <button className="px-8 py-3 mt-6 text-white bg-black rounded">
+          Save
+        </button>
+      </div>
+    </section>
+  );
+};
 
 export default ProfileNotificationPage;
