7b407934c8f7f5548ee8f28d1f88942eae4fa178
BasemYahia402 <basemyahia70@gmail.com>
2024-03-24 02:44:04 +0200

    Add ProfileHeader and SwitchComponent to ProfilePrivacyPage;

--- src/pages/ProfilePrivacyPage.jsx
+++ src/pages/ProfilePrivacyPage.jsx
@@ -1,9 +1,29 @@
+import ProfileHeader from "../shared/ProfileHeader";
+import SwitchComponent from "../shared/SwitichComponent";
+/* eslint-disable */
 const ProfilePrivacyPage = () => {
-    return (
-        <div>
-            <h1 className="mt-5 text-center">Profile Privacy</h1>
+  const title = "Privacy";
+  const description = "Modify your privacy settings here.";
+  return (
+    <section className="py-8 border border-b-0 border-gray-400 ">
+      <ProfileHeader title={title} description={description} />
+      <div className="px-3 py-8 font-bold leading-8 md:px-5 lg:px-10 md:text-xl">
+        <h3 className="mb-4">Profile page settings:</h3>
+        <div className="flex items-center justify-between mb-4">
+          <p>Share social platforms on my profile.</p>
+          <SwitchComponent />
         </div>
-    );
-}
+        <div className="flex items-center justify-between mb-4">
+          <p>Share social platforms on my profile.</p>
+          <SwitchComponent />
+        </div>
+
+        <button className="px-8 py-3 mt-12 text-white bg-black rounded">
+          Save
+        </button>
+      </div>
+    </section>
+  );
+};
 
 export default ProfilePrivacyPage;
