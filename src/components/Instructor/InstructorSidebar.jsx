c2945f96d285844b86a7609aae900c339dad298e
FrontEndDevo <mylinkacco@gmail.com>
2024-04-11 17:12:46 +0200

    Add InstructorSidebar component and import it in InstructorPage

---
+++ src/components/Instructor/InstructorSidebar.jsx
@@ -0,0 +1,5 @@
+const InstructorSidebar = () => {
+  return <aside>InstructorSidebar</aside>;
+};
+
+export default InstructorSidebar;
--- src/pages/Instructor/InstructorPage.jsx
+++ src/pages/Instructor/InstructorPage.jsx
@@ -1,10 +1,12 @@
 import InstructorCourses from "../../components/Instructor/InstructorCourses";
+import InstructorSidebar from "../../components/Instructor/InstructorSideBar";
 
 const InstructorPage = () => {
   return (
-    <>
+    <div>
+      <InstructorSidebar />
       <InstructorCourses />
-    </>
+    </div>
   );
 };
 
