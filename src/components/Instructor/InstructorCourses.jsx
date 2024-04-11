1e0cfa263da7c45c9ec38870d11fd1a7b004e469
FrontEndDevo <mylinkacco@gmail.com>
2024-04-11 19:54:34 +0200

    Add instructor option state and handler

--- src/components/Instructor/InstructorCourses.jsx
+++ src/components/Instructor/InstructorCourses.jsx
@@ -7,6 +7,7 @@ import img6 from "../../assets/images/homepage/course_6.jpg";
 import img7 from "../../assets/images/homepage/course_7.jpg";
 import InstructorCourseCard from "../../shared/Courses/InstructorCourseCard";
 import InstructorSidebar from "../../components/Instructor/InstructorSideBar";
+import { useState } from "react";
 
 // Temp data: replace with actual data from API
 const instructorCourses = [
@@ -83,16 +84,34 @@ const instructorCourses = [
 ];
 
 const InstructorCourses = () => {
+  const [instructorOption, setInstructorOption] = useState("courses");
+
+  const handleInstructorOption = (option) => {
+    setInstructorOption(option);
+  };
+
+  // Map through the instructorCourses array and render an InstructorCourseCard component for each course.
   const instructorCoursesList = instructorCourses.map((course, index) => (
     <InstructorCourseCard key={index} {...course} />
   ));
 
   return (
     <section className="flex">
-      <InstructorSidebar />
-      <ul className="flex px-10 py-20 items-center gap-x-6 gap-y-16 flex-wrap">
-        {instructorCoursesList}
-      </ul>
+      <InstructorSidebar
+        selectedOption={instructorOption}
+        getInstructorOption={handleInstructorOption}
+      />
+      <div className="px-10 py-20">
+        <div className="flex items-center justify-between mb-10">
+          <h2 className="capitalize text-2xl font-bold">
+            my {instructorOption}
+          </h2>
+          {/* Sort */}
+        </div>
+        <ul className="flex items-center gap-x-6 gap-y-16 flex-wrap">
+          {instructorCoursesList}
+        </ul>
+      </div>
     </section>
   );
 };
