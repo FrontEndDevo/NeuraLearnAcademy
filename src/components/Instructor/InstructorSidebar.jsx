5b6d7442c4a850833f0e5ce9fc888ba3408c2b66
FrontEndDevo <mylinkacco@gmail.com>
2024-04-11 18:33:06 +0200

    Render options to InstructorSidebar

--- src/components/Instructor/InstructorSidebar.jsx
+++ src/components/Instructor/InstructorSidebar.jsx
@@ -1,5 +1,41 @@
+import {
+  faChartLine,
+  faCommentDots,
+  faComputer,
+  faFileSignature,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+const InstructorSidebarOptions = [
+  { icon: faComputer, name: "courses" },
+  { icon: faChartLine, name: "activity" },
+  {
+    icon: faFileSignature,
+    name: "assignments",
+  },
+  { icon: faCommentDots, name: "chat" },
+];
 const InstructorSidebar = () => {
-  return <aside>InstructorSidebar</aside>;
+  const renderInstructorSidebarOptions = InstructorSidebarOptions.map(
+    (option, index) => (
+      <div
+        key={index}
+        className="flex items-center flex-col gap-2 justify-center w-full my-6 cursor-pointer"
+      >
+        <FontAwesomeIcon
+          icon={option.icon}
+          className="w-10 h-10 duration-200 hover:text-primary-500"
+        />
+        <h3 className="font-bold text-sm capitalize">{option.name}</h3>
+      </div>
+    )
+  );
+
+  return (
+    <aside className="w-[10vw] overflow-hidden h-screen bg-white px-2 py-20 border-r-2 shadow-xl">
+      {renderInstructorSidebarOptions}
+    </aside>
+  );
 };
 
 export default InstructorSidebar;
