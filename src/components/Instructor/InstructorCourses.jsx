cc8ae2df2b86b829097d363c7ffae92f383a3fee
FrontEndDevo <mylinkacco@gmail.com>
2024-04-11 15:17:15 +0200

    Prepare the temporary array of courses

--- src/components/Instructor/InstructorCourses.jsx
+++ src/components/Instructor/InstructorCourses.jsx
@@ -1,3 +1,85 @@
+import img1 from "../../assets/images/homepage/course_1.jpg";
+import img2 from "../../assets/images/homepage/course_2.jpg";
+import img3 from "../../assets/images/homepage/course_3.jpg";
+import img4 from "../../assets/images/homepage/course_4.jpg";
+import img5 from "../../assets/images/homepage/course_5.jpg";
+import img6 from "../../assets/images/homepage/course_6.jpg";
+import img7 from "../../assets/images/homepage/course_7.jpg";
+
+// Temp data: replace with actual data from API
+const instructorCourses = [
+  {
+    img: img1,
+    title: "machine learning",
+    videos: 186,
+    sections: 25,
+    quizzes: 16,
+    category: "Web Development",
+    students: 82342,
+    price: 50,
+  },
+  {
+    img: img2,
+    title: "machine learning",
+    videos: 186,
+    sections: 25,
+    quizzes: 16,
+    category: "Web Development",
+    students: 82342,
+    price: 50,
+  },
+  {
+    img: img3,
+    title: "machine learning",
+    videos: 186,
+    sections: 25,
+    quizzes: 16,
+    category: "Web Development",
+    students: 82342,
+    price: 50,
+  },
+  {
+    img: img4,
+    title: "machine learning",
+    videos: 186,
+    sections: 25,
+    quizzes: 16,
+    category: "Web Development",
+    students: 82342,
+    price: 50,
+  },
+  {
+    img: img5,
+    title: "machine learning",
+    videos: 186,
+    sections: 25,
+    quizzes: 16,
+    category: "Web Development",
+    students: 82342,
+    price: 50,
+  },
+  {
+    img: img6,
+    title: "machine learning",
+    videos: 186,
+    sections: 25,
+    quizzes: 16,
+    category: "Web Development",
+    students: 82342,
+    price: 50,
+  },
+  {
+    img: img7,
+    title: "machine learning",
+    videos: 186,
+    sections: 25,
+    quizzes: 16,
+    category: "Web Development",
+    students: 82342,
+    price: 50,
+  },
+];
+
 const InstructorCourses = () => {
   return (
     <section>
