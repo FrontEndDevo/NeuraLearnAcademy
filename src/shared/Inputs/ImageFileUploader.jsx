08b6a19be2cb4c0e8d0eef24e0ee600ae660ed1f
FrontEndDevo <mylinkacco@gmail.com>
2024-04-15 14:23:15 +0200

    Add ImageFileUploader component for uploading image files

---
+++ src/shared/Inputs/ImageFileUploader.jsx
@@ -0,0 +1,32 @@
+import propsTypes from "prop-types";
+
+// A component that allows users to upload an image file.
+const ImageFileUploader = (props) => {
+  const handleUploadImage = (event) => {
+    const file = event.target.files[0];
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      props.getImage(reader.result);
+    };
+
+    if (file) reader.readAsDataURL(file);
+  };
+
+  return (
+    <input
+      type="file"
+      name={props.name}
+      id={props.name}
+      accept="image/*" // Accepts all image formats
+      className="hidden"
+      onChange={handleUploadImage}
+    />
+  );
+};
+ImageFileUploader.propTypes = {
+  name: propsTypes.string.isRequired,
+  getImage: propsTypes.func.isRequired,
+};
+
+export default ImageFileUploader;
