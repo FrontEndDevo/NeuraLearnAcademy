34630041a413cd5b1b21fac0fbb1f2e254646bdb
BasemYahia402 <basemyahia70@gmail.com>
2024-04-18 16:31:59 +0200

    Subscribe authSlice to redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import searchCoursesReducer from "./slices/searchCourses";
+import authSlice from "./slices/authentication/auth";
 export const store = configureStore({
   reducer: {
     searchCourses: searchCoursesReducer,
+    userAuth: authSlice,
   },
 });
