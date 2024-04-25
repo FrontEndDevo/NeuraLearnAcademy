db551abb37cd5ecd3f47bf36106465c9a972a4d6
FrontEndDevo <mylinkacco@gmail.com>
2024-04-25 17:46:25 +0200

    Refactor Failed component to include error prop in Failed.jsx

--- src/components/Registration/SuccessFailed/Failed.jsx
+++ src/components/Registration/SuccessFailed/Failed.jsx
@@ -4,12 +4,12 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useDispatch } from "react-redux";
 import { closeModal } from "../../../redux/slices/Instructor/OpenClose";
 import BlurModal from "../../../shared/BlurModal";
-
-const Failed = () => {
+import PropTypes from "prop-types";
+const Failed = ({ error }) => {
   // Close the create course modal:
   const dispatch = useDispatch();
 
-  const handleCloseSuccessFailedModal = () => {
+  const handleCloseFailedModal = () => {
     dispatch(closeModal());
   };
 
@@ -20,16 +20,14 @@ const Failed = () => {
       <div className="p-10 text-white bg-[#ee5c4d] text-center z-50 md:w-1/2 xl:w-1/3 2xl:w-1/5 flex flex-col rounded border border-neutral-300 shadow items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <FontAwesomeIcon
           icon={faXmark}
-          onClick={handleCloseSuccessFailedModal}
+          onClick={handleCloseFailedModal}
           className="absolute text-gray-200 top-4 right-4 text-xl cursor-pointer duration-200 hover:text-gray-400"
         />
 
         <h2 className="text-3xl font-semibold mb-6">
           Oops! Something went wrong.
         </h2>
-        <p className="text-gray-200 my-2">
-          Your account has been created successfully.
-        </p>
+        <p className="text-gray-200 font-semibold text-lg my-2">{error}</p>
 
         <FontAwesomeIcon
           icon={faCircleXmark}
@@ -37,7 +35,7 @@ const Failed = () => {
         />
 
         <button
-          onClick={handleCloseSuccessFailedModal}
+          onClick={handleCloseFailedModal}
           className="py-2 mt-4 font-bold text-white capitalize duration-200 bg-[#a3352d] rounded-full outline-none md:mt-8 px-14 hover:bg-[#8a2820]"
         >
           Try Again
@@ -45,6 +43,10 @@ const Failed = () => {
       </div>
     </>
   );
+};
+
+Failed.propsTypes = {
+  error: PropTypes.string.isRequired,
 };
 
 export default Failed;
