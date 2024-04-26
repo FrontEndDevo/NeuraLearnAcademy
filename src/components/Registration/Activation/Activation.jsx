732ad39e48b1c1f72d32ed02ac4000f8d5c3b133
FrontEndDevo <mylinkacco@gmail.com>
2024-04-26 14:48:40 +0300

    Add the SucessFailedBox component to the Activation one to handle the message that should appear to user after verify the account

--- src/components/Registration/Activation/Activation.jsx
+++ src/components/Registration/Activation/Activation.jsx
@@ -7,6 +7,8 @@ import { faFaceSmileWink } from "@fortawesome/free-regular-svg-icons";
 import logo from "../../../assets/images/LoginSigin/logo.png";
 import { useState } from "react";
 import RegisterButton from "../../../shared/Registration/RegisterButton";
+import SucessFailedBox from "../../../shared/Registration/SucessFailedBox";
+import { openModal } from "../../../redux/slices/Instructor/OpenClose";
 const Activation = () => {
   const [spinner, setSpinner] = useState(false);
 
@@ -22,6 +24,7 @@ const Activation = () => {
     setSpinner(true);
     await activation(dispatch, uid, token);
     setSpinner(false);
+    dispatch(openModal("registration"));
     // navigate("/");
   };
 
@@ -30,46 +33,52 @@ const Activation = () => {
     "p-4 text-xl md:text-3xl text-indigo-600 border-4 border-indigo-600 rounded-full";
 
   return (
-    <div className="container flex flex-col items-center justify-center py-20 md:w-screen md:h-screen">
-      <img src={logo} alt="Neura Learn Academy" className="w-40 h-40 mb-10" />
-      <div className="px-10 py-6 text-center bg-gray-200 rounded">
-        <div className="flex items-center justify-center mb-4">
-          <h1 className="px-4 py-2 text-lg font-bold border-b-2 border-indigo-600 rounded-b-lg md:text-2xl w-fit font-playfair">
-            Activate your Account:
-          </h1>
-        </div>
+    <>
+      <div className="container flex flex-col items-center justify-center py-20 md:w-screen md:h-screen">
+        <img src={logo} alt="Neura Learn Academy" className="w-40 h-40 mb-10" />
+        <div className="px-10 py-6 text-center bg-gray-200 rounded">
+          <div className="flex items-center justify-center mb-4">
+            <h1 className="px-4 py-2 text-lg font-bold border-b-2 border-indigo-600 rounded-b-lg md:text-2xl w-fit font-playfair">
+              Activate your Account:
+            </h1>
+          </div>
 
-        <div className="grid items-center justify-around grid-cols-2 gap-4 mb-10 text-sm font-bold capitalize md:text-base md:flex">
-          <div className={stepsParentDiv}>
-            <span>create account</span>
-            <FontAwesomeIcon icon={faCheck} className={stepsClasses} />
+          <div className="grid items-center justify-around grid-cols-2 gap-4 mb-10 text-sm font-bold capitalize md:text-base md:flex">
+            <div className={stepsParentDiv}>
+              <span>create account</span>
+              <FontAwesomeIcon icon={faCheck} className={stepsClasses} />
+            </div>
+            <div className={stepsParentDiv}>
+              <span>verify email</span>
+              <FontAwesomeIcon icon={faEnvelope} className={stepsClasses} />
+            </div>
+            <div className={`${stepsParentDiv} opacity-50 col-span-2`}>
+              <span>good luck</span>
+              <FontAwesomeIcon
+                icon={faFaceSmileWink}
+                className={stepsClasses}
+              />
+            </div>
           </div>
-          <div className={stepsParentDiv}>
-            <span>verify email</span>
-            <FontAwesomeIcon icon={faEnvelope} className={stepsClasses} />
-          </div>
-          <div className={`${stepsParentDiv} opacity-50 col-span-2`}>
-            <span>good luck</span>
-            <FontAwesomeIcon icon={faFaceSmileWink} className={stepsClasses} />
-          </div>
-        </div>
-        <div className="text-xs font-semibold tracking-wide md:text-lg">
-          <p className="mb-2">
-            Thank you for registering with us. You are one step away from
-            enjoying the most famous online educational courses.
-          </p>
-          <p className="mb-2">
-            In order to activate your account please click the button below
-          </p>
+          <div className="text-xs font-semibold tracking-wide md:text-lg">
+            <p className="mb-2">
+              Thank you for registering with us. You are one step away from
+              enjoying the most famous online educational courses.
+            </p>
+            <p className="mb-2">
+              In order to activate your account please click the button below
+            </p>
 
-          <RegisterButton
-            keyword="verify"
-            isLoading={spinner}
-            clickButton={handleVerifyAccount}
-          />
+            <RegisterButton
+              keyword="verify"
+              isLoading={spinner}
+              clickButton={handleVerifyAccount}
+            />
+          </div>
         </div>
       </div>
-    </div>
+      <SucessFailedBox page="activation" />
+    </>
   );
 };
 
