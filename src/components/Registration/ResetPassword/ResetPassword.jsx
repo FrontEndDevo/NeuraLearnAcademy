b3a7dbb9a0fcb7f92ce3317de7c59a9708015ba9
FrontEndDevo <mylinkacco@gmail.com>
2024-04-26 17:44:08 +0300

    Refactor ResetPassword and ResetPasswordConfirm components to include openModal action and SucessFailedBox component

--- src/components/Registration/ResetPassword/ResetPassword.jsx
+++ src/components/Registration/ResetPassword/ResetPassword.jsx
@@ -4,6 +4,8 @@ import { reset_password } from "../../../redux/actions/auth-methods";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import NeuraLearnAcademy from "../../../shared/NeuraLearnAcademy";
+import { openModal } from "../../../redux/slices/Instructor/OpenClose";
+import SucessFailedBox from "../../../shared/Registration/SucessFailedBox";
 
 const ResetPassword = () => {
   const [spinner, setSpinner] = useState(false);
@@ -16,55 +18,64 @@ const ResetPassword = () => {
     e.preventDefault();
     await reset_password(dispatch, emailRef.current.value);
     setSpinner(false);
+    dispatch(openModal("registration"));
   };
 
   return (
-    <div className="text-neutral-900 flex flex-col items-center justify-center py-20 md:w-screen md:h-screen">
-      <NeuraLearnAcademy />
+    <>
+      <div className="flex flex-col items-center justify-center py-20 text-neutral-900 md:w-screen md:h-screen">
+        <NeuraLearnAcademy />
 
-      <div className="mx-4 px-10 py-6 md:px-20 md:py-12 text-center border-2 border-indigo-600 rounded shadow-innerwhite">
-        <div className="mb-6">
-          <h1 className="text-3xl md:text-6xl mb-4 font-extrabold">
-            Reset Password
-          </h1>
-          <p className="text-sm">
-            Provide the email address associated with your account to recover
-            your password.
-          </p>
-        </div>
+        <div className="px-10 py-6 mx-4 text-center bg-white border-2 border-indigo-600 rounded md:px-20 md:py-12 shadow-innerwhite">
+          <div className="mb-6">
+            <h1 className="mb-4 text-3xl font-extrabold md:text-6xl">
+              Reset Password
+            </h1>
+            <p className="text-sm">
+              Provide the email address associated with your account to recover
+              your password.
+            </p>
+          </div>
 
-        <div className="flex items-center justify-center gap-2">
-          <input
-            type="email"
-            id="email"
-            ref={emailRef}
-            placeholder="Email"
-            className="w-full py-2 pl-2 border border-gray-300 focus:border-indigo-700 caret-indigo-700 outline-none duration-300 sm:w-96 md:w-96"
-            required
+          <div className="flex items-center justify-center gap-2">
+            <input
+              type="email"
+              id="email"
+              ref={emailRef}
+              placeholder="Email"
+              className="w-full py-2 pl-2 duration-300 border border-gray-300 outline-none focus:border-indigo-700 caret-indigo-700 sm:w-96 md:w-96"
+              required
+            />
+          </div>
+
+          <RegisterButton
+            keyword="reset password"
+            isLoading={spinner}
+            clickButton={handleResetPassword}
           />
-        </div>
-
-        <RegisterButton
-          keyword="reset password"
-          isLoading={spinner}
-          clickButton={handleResetPassword}
-        />
-        <div className="flex items-center justify-center mt-4 gap-40">
-          <Link
-            to="/login"
-            className="text-gray-500 hover:text-gray-800 duration-200"
-          >
-            Login
-          </Link>
-          <Link
-            to="/login"
-            className="text-gray-500 hover:text-gray-800 duration-200"
-          >
-            Signup
-          </Link>
+          <div className="flex items-center justify-center gap-40 mt-4">
+            <Link
+              to="/login"
+              className="text-gray-500 duration-200 hover:text-gray-800"
+            >
+              Login
+            </Link>
+            <Link
+              to="/login"
+              className="text-gray-500 duration-200 hover:text-gray-800"
+            >
+              Signup
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+
+      <SucessFailedBox
+        page="reset_password"
+        navigatePage="/login"
+        successMessage={`Your password has been reset. An email has been sent to ${emailRef.current.value}. Please check your email for reset link.`}
+      />
+    </>
   );
 };
 
