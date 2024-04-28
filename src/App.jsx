67ec562156774336855a34309ea363fa840b72af
FrontEndDevo <mylinkacco@gmail.com>
2024-04-29 00:27:26 +0300

    Remove unused import code in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
