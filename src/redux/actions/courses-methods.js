5942c34704d6678eeeac5f6f4a3b9036c56fb392
BasemYahia402 <basemyahia70@gmail.com>
2024-05-03 10:22:15 +0300

    Add two functions for getSubjectCourses and getInstructorCourses

--- src/redux/actions/courses-methods.js
+++ src/redux/actions/courses-methods.js
@@ -1,1 +1,59 @@
+import axios from "axios";
+import {
+  GETINSTRUCTORCOURSES_FAIL,
+  GETINSTRUCTORCOURSES_SUCCESS,
+  GETSUBJECTCOURSES_FAIL,
+  GETSUBJECTCOURSES_SUCCESS,
+} from "../slices/courses/courses-slice";
+import {
+  GETINSTRUCTORCOURSES_ERROR,
+  GETSUBJECTCOURSES_ERROR,
+} from "../slices/courses/errors";
 
+export async function getSubjectCourses(dispatch, access) {
+  if (access) {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `JWT ${access}`,
+        Accept: "application/json",
+      },
+    };
+
+    try {
+      const res = await axios.get(
+        `http://localhost:8000/api/courses/subjects/`,
+        config
+      );
+      dispatch(GETSUBJECTCOURSES_SUCCESS(res.data));
+    } catch (err) {
+      dispatch(GETSUBJECTCOURSES_FAIL());
+      dispatch(GETSUBJECTCOURSES_ERROR(err.response.data));
+    }
+  }
+}
+export async function getInstructorCourses(
+  dispatch,
+  access,
+  api = `http://localhost:8000/api/courses/mine/?limit=x&offset=x`
+) {
+  if (access) {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `JWT ${access}`,
+        Accept: "application/json",
+      },
+    };
+
+    try {
+      const res = await axios.get(api, config);
+      console.log(res);
+      dispatch(GETINSTRUCTORCOURSES_SUCCESS(res.data));
+    } catch (err) {
+      console.log(err);
+      dispatch(GETINSTRUCTORCOURSES_FAIL());
+      dispatch(GETINSTRUCTORCOURSES_ERROR(err.response.data));
+    }
+  }
+}
