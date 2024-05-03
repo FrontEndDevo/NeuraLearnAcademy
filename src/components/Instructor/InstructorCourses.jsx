c89af5ba16d2edd6812a3d1af3c9c260a05f9f2a
BasemYahia402 <basemyahia70@gmail.com>
2024-05-03 10:30:09 +0300

    Get courses from backend use api

--- src/components/Instructor/InstructorCourses.jsx
+++ src/components/Instructor/InstructorCourses.jsx
@@ -1,100 +1,31 @@
-import { useState } from "react";
-import img1 from "../../assets/images/homepage/course_1.jpg";
-import img2 from "../../assets/images/homepage/course_2.jpg";
-import img3 from "../../assets/images/homepage/course_3.jpg";
-import img4 from "../../assets/images/homepage/course_4.jpg";
-import img5 from "../../assets/images/homepage/course_5.jpg";
-import img6 from "../../assets/images/homepage/course_6.jpg";
-import img7 from "../../assets/images/homepage/course_7.jpg";
+import { useEffect, useState } from "react";
 import InstructorCourseCard from "../../shared/Courses/InstructorCourseCard";
 import InstructorSidebar from "../../components/Instructor/InstructorSideBar";
 import DefaultInstructorCourse from "../../shared/Courses/DefaultInstructorCourse";
-
-// Temp data: replace with actual data from API
-const instructorCourses = [
-  {
-    img: img1,
-    title: "machine learning",
-    videos: 186,
-    sections: 25,
-    quizzes: 16,
-    category: "Web Development",
-    students: 82342,
-    price: 50,
-  },
-  {
-    img: img2,
-    title: "machine learning",
-    videos: 186,
-    sections: 25,
-    quizzes: 16,
-    category: "Web Development",
-    students: 82342,
-    price: 50,
-  },
-  {
-    img: img3,
-    title: "machine learning",
-    videos: 186,
-    sections: 25,
-    quizzes: 16,
-    category: "Web Development",
-    students: 82342,
-    price: 50,
-  },
-  {
-    img: img4,
-    title: "machine learning",
-    videos: 186,
-    sections: 25,
-    quizzes: 16,
-    category: "Web Development",
-    students: 82342,
-    price: 50,
-  },
-  {
-    img: img5,
-    title: "machine learning",
-    videos: 186,
-    sections: 25,
-    quizzes: 16,
-    category: "Web Development",
-    students: 82342,
-    price: 50,
-  },
-  {
-    img: img6,
-    title: "machine learning",
-    videos: 186,
-    sections: 25,
-    quizzes: 16,
-    category: "Web Development",
-    students: 82342,
-    price: 50,
-  },
-  {
-    img: img7,
-    title: "machine learning",
-    videos: 186,
-    sections: 25,
-    quizzes: 16,
-    category: "Web Development",
-    students: 82342,
-    price: 50,
-  },
-];
+import { useDispatch, useSelector } from "react-redux";
+import { getInstructorCourses } from "../../redux/actions/courses-methods";
 
 const InstructorCourses = () => {
+  const instructorCourses = useSelector(
+    (state) => state.courseData.instructorCourses
+  );
+  const access = useSelector((state) => state.userAuth.access);
+  const next = instructorCourses?.next;
+  const previous = instructorCourses?.previous;
   const [instructorOption, setInstructorOption] = useState("courses");
+  const dispatch = useDispatch();
+  useEffect(() => {
+    getInstructorCourses(dispatch, access, next);
+  }, []);
 
   const handleInstructorOption = (option) => {
     setInstructorOption(option);
   };
 
   // Map through the instructorCourses array and render an InstructorCourseCard component for each course.
-  const instructorCoursesList = instructorCourses.map((course, index) => (
-    <InstructorCourseCard key={index} {...course} />
-  ));
+  const instructorCoursesList = instructorCourses?.results?.map(
+    (course, index) => <InstructorCourseCard key={index} {...course} />
+  );
 
   return (
     <section className="flex">
@@ -107,7 +38,6 @@ const InstructorCourses = () => {
           <h2 className="text-2xl font-bold capitalize">
             my {instructorOption}
           </h2>
-          {/* Sort */}
         </div>
 
         <ul className="flex flex-wrap items-center gap-x-6 gap-y-16">
