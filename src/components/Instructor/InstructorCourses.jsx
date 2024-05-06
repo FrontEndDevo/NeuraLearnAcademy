0f15a1a25bb4acf2e891e654f8d911de39050c3b
FrontEndDevo <mylinkacco@gmail.com>
2024-05-06 21:27:00 +0300

    Add pagination functionality in InstructorCourses component

--- src/components/Instructor/InstructorCourses.jsx
+++ src/components/Instructor/InstructorCourses.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import img1 from "../../assets/images/homepage/course_1.jpg";
 import img2 from "../../assets/images/homepage/course_2.jpg";
 import img3 from "../../assets/images/homepage/course_3.jpg";
@@ -9,6 +9,7 @@ import img7 from "../../assets/images/homepage/course_7.jpg";
 import InstructorCourseCard from "../../shared/Courses/InstructorCourseCard";
 import InstructorSidebar from "../../components/Instructor/InstructorSideBar";
 import DefaultInstructorCourse from "../../shared/Courses/DefaultInstructorCourse";
+import Pagination from "../../shared/Pagination";
 
 // Temp data: replace with actual data from API
 const instructorCourses = [
@@ -113,26 +114,6 @@ const instructorCourses = [
     price: 50,
   },
   {
-    img: img4,
-    title: "machine learning",
-    videos: 186,
-    sections: 25,
-    quizzes: 16,
-    category: "Web Development",
-    students: 82342,
-    price: 50,
-  },
-  {
-    img: img5,
-    title: "machine learning",
-    videos: 186,
-    sections: 25,
-    quizzes: 16,
-    category: "Web Development",
-    students: 82342,
-    price: 50,
-  },
-  {
     img: img6,
     title: "machine learning",
     videos: 186,
@@ -152,19 +133,57 @@ const instructorCourses = [
     students: 82342,
     price: 50,
   },
+  {
+    img: img4,
+    title: "machine learning",
+    videos: 186,
+    sections: 25,
+    quizzes: 16,
+    category: "Web Development",
+    students: 82342,
+    price: 50,
+  },
+  {
+    img: img5,
+    title: "machine learning",
+    videos: 186,
+    sections: 25,
+    quizzes: 16,
+    category: "Web Development",
+    students: 82342,
+    price: 50,
+  },
 ];
 
 const InstructorCourses = () => {
   const [instructorOption, setInstructorOption] = useState("courses");
 
+  // The number of elements to be rendered per page.
+  const elementsPerPage = 7; // Add to this value 1 (DefaultInstructorCourse component)
+
+  const [paginationIndices, setPaginationIndices] = useState({
+    start: 0,
+    end: elementsPerPage,
+  });
+
+  // Memorize the pagination function to avoid re-rendering.
+  const memorizedInstructorCoursesPagination = useCallback((cur) => {
+    // Calc the first and last product index that should be rendered.
+    const startIndex = (cur - 1) * elementsPerPage;
+    const endIndex = startIndex + elementsPerPage;
+
+    setPaginationIndices({ start: startIndex, end: endIndex });
+  }, []);
+
+  // Get the selected option from the sidebar.
   const handleInstructorOption = (option) => {
     setInstructorOption(option);
   };
 
   // Map through the instructorCourses array and render an InstructorCourseCard component for each course.
-  const instructorCoursesList = instructorCourses.map((course, index) => (
-    <InstructorCourseCard key={index} {...course} />
-  ));
+  const instructorCoursesList = instructorCourses
+    .slice(paginationIndices.start, paginationIndices.end)
+    .map((course, index) => <InstructorCourseCard key={index} {...course} />);
 
   return (
     <section className="flex flex-col lg:flex-row">
@@ -179,10 +198,15 @@ const InstructorCourses = () => {
           </h2>
         </div>
 
-        <ul className="grid grid-cols-1 md:grid-cols-2  xl:grid-cols-3 2xl:grid-cols-4 gap-6">
-          <DefaultInstructorCourse />
-          {instructorCoursesList}
-        </ul>
+        <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-6 auto-rows-fr">
+  <DefaultInstructorCourse />
+  {instructorCoursesList}
+</ul>
+        <Pagination
+          elementsPerPage={elementsPerPage}
+          length={instructorCourses.length}
+          getCurrentPage={memorizedInstructorCoursesPagination}
+        />
       </div>
     </section>
   );
