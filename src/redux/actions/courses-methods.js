74637b76a14ba57106f3d92f9fd8ed3c0a741720
FrontEndDevo <mylinkacco@gmail.com>
2024-05-06 23:11:07 +0300

    Add public_courses function to fetch public courses from API

--- src/redux/actions/courses-methods.js
+++ src/redux/actions/courses-methods.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+import { setPublicCourses } from "../slices/courses/courses-slice";
+import { setPublicCoursesError } from "../slices/courses/courses-errors";
 
+export const public_courses = async (dispatch) => {
+  try {
+    const res = await axios.get(`http://localhost:8000/api/public/courses/`);
+    dispatch(setPublicCourses(res.data.results));
+  } catch (err) {
+    dispatch(setPublicCoursesError());
+  }
+};
