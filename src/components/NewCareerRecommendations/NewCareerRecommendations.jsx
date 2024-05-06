b4d1ad24597f835ec846846b0feeed1ece0ac7a1
FrontEndDevo <mylinkacco@gmail.com>
2024-05-07 00:36:11 +0300

    Refactor NewCareerRecommendations component to use fetch publicCourses and use it instead of temp data

--- src/components/NewCareerRecommendations/NewCareerRecommendations.jsx
+++ src/components/NewCareerRecommendations/NewCareerRecommendations.jsx
@@ -1,16 +1,12 @@
-import course_one from "../../assets/images/homepage/course_1.jpg";
-import course_two from "../../assets/images/homepage/course_2.jpg";
-import course_three from "../../assets/images/homepage/course_3.jpg";
-import course_four from "../../assets/images/homepage/course_4.jpg";
-import course_five from "../../assets/images/homepage/course_5.jpg";
 import Slider from "react-slick";
 import CourseCard from "../../shared/Courses/CourseCard";
+import { useSelector } from "react-redux";
 
 const settings = {
   dots: true,
   infinite: true,
-  speed: 500,
-  slidesToShow: 5,
+  speed: 1000,
+  slidesToShow: 4,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 2000,
@@ -43,102 +39,20 @@ const settings = {
   ],
 };
 
-// This is a temporary array of objects (courses) until fetch all of these from DB.
-const recomendedCourses = [
-  {
-    title: "The complete course of Bussines for beginners",
-    img: course_one,
-    instructor: "Adel nsiem",
-    price: 30,
-    rate: 4.7,
-    category: "web programming",
-  },
-  {
-    title: "The complete course of Balence",
-    img: course_two,
-    instructor: "Adel nsiem",
-    price: 30,
-    rate: 4.7,
-    category: "web programming",
-  },
-  {
-    title: "The complete course Swintific Rwriting",
-    img: course_three,
-    instructor: "Adel nsiem",
-    price: 30,
-    rate: 4.7,
-    category: "web programming",
-  },
-  {
-    title: "The complete course of Machine Learing",
-    img: course_four,
-    instructor: "Adel nsiem",
-    price: 30,
-    rate: 4.7,
-    category: "web programming",
-  },
-  {
-    title: "The complete course of Artfical for beginners",
-    img: course_five,
-    instructor: "Adel nsiem",
-    price: 30,
-    rate: 4.7,
-    category: "web programming",
-  },
-  {
-    title: "The complete course of Bussines for beginners",
-    img: course_one,
-    instructor: "Adel nsiem",
-    price: 30,
-    rate: 4.7,
-    category: "web programming",
-  },
-  {
-    title: "The complete course of Balence",
-    img: course_two,
-    instructor: "Adel nsiem",
-    price: 30,
-    rate: 4.7,
-    category: "web programming",
-  },
-  {
-    title: "The complete course Swintific Rwriting",
-    img: course_three,
-    instructor: "Adel nsiem",
-    price: 30,
-    rate: 4.7,
-    category: "web programming",
-  },
-  {
-    title: "The complete course of Machine Learing",
-    img: course_four,
-    instructor: "Adel nsiem",
-    price: 30,
-    rate: 4.7,
-    category: "web programming",
-  },
-  {
-    title: "The complete course of Artfical for beginners",
-    img: course_five,
-    instructor: "Adel nsiem",
-    price: 30,
-    rate: 4.7,
-    category: "web programming",
-  },
-];
 const NewCareerRecommendations = () => {
+  const publicCourses = useSelector((state) => state.courses.publicCourses);
+
   // Render all courses inside a slider:
   const renderedCourses = (
     <Slider {...settings}>
-      {recomendedCourses.map((item, i) => (
+      {publicCourses.map((item, i) => (
         <CourseCard
           key={i}
-          image={item.img}
+          image={item.image}
           title={item.title}
           instructor={item.instructor}
           category={item.category}
-          price={item.price}
-          rate={item.rate}
+          price={+item.price}
         />
       ))}
     </Slider>
