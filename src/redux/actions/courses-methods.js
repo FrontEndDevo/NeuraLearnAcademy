3de4dc7c8cd214a6446a6ca68ffce8593a3aec2a
FrontEndDevo <mylinkacco@gmail.com>
2024-05-07 00:51:01 +0300

    Refactor API URLs to use environment variables (dot env file)

--- src/redux/actions/courses-methods.js
+++ src/redux/actions/courses-methods.js
@@ -4,7 +4,9 @@ import { setPublicCoursesError } from "../slices/courses/courses-errors";
 
 export const public_courses = async (dispatch) => {
   try {
-    const res = await axios.get(`http://localhost:8000/api/public/courses/`);
+    const res = await axios.get(
+      import.meta.env.VITE_API_URL + "/api/public/courses/"
+    );
     dispatch(setPublicCourses(res.data.results));
   } catch (err) {
     dispatch(setPublicCoursesError());
