9ffd8e94b935ddbf4377150d42161a77c50e72ed
karim Abdelazim <ka1465@fayoum.edu.eg>
2024-05-10 01:41:34 +0300

    edit the input and edit the margin and padding in small screen.

--- src/components/Instructor/CreateSections/CreateNewSection.jsx
+++ src/components/Instructor/CreateSections/CreateNewSection.jsx
@@ -21,33 +21,30 @@ const CreateNewSection = () => {
         dispatch(closeModal());
     };
     // Handle the saving of the course information:
-    const handleSavingCourseInformation = () => {
-        // Get the values from the input fields using useRef hooks:
-        const title = titleRef.current.value.trim();
-        const description = descriptionRef.current.value.trim();
-        const price = +priceRef.current.value;
+    // const handleSavingCourseInformation = () => {
+    //     // Get the values from the input fields using useRef hooks:
+    //     const title = titleRef.current.value.trim();
+    //     const description = descriptionRef.current.value.trim();
+    //     const price = +priceRef.current.value;
 
-        if (!title || !price) {
-            setMissingError(true);
-        } else {
-            // Do something with this course information like sending it to the server.
-            console.log(
-                `Title: ${title} Description: ${description} Price: ${price} Category: ${category} Thumbnail: ${thumbnail}`
-            );
-            // Close the modal:
-            handleCloseCreateCourse();
-        }
-    };
+    //     if (!title || !price) {
+    //         setMissingError(true);
+    //     } else {
+    //         // Do something with this course information like sending it to the server.
+    //         console.log(
+    //             `Title: ${title} Description: ${description} Price: ${price} Category: ${category} Thumbnail: ${thumbnail}`
+    //         );
+    //         // Close the modal:
+    //         handleCloseCreateCourse();
+    //     }
+    // };
 
-    // Classes for styling:
-    const labelClasses = "block mb-1 text-sm lg:text-base lg:font-semibold";
-    const inputClasses =
-        "p-2 text-base rounded-sm w-full mb-2 outline-none caret-indigo-800 border border-black border-opacity-80";
+
     return (
         <>
             <BlurModal />
 
-            <div className="lg:w-[50vw] w-[95vw] h-[75vh] lg:h-[50vh]  z-50 bg-white rounded-lg fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 shadow-xl">
+            <div className="lg:w-[50vw] w-[80vw] h-[50vh] lg:h-[45vh]  z-50 bg-white rounded-lg fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 shadow-xl">
                 <div className="flex justify-between items-center w-full px-4 py-4 bg-zinc-100 rounded-tl-[10px] rounded-tr-[10px] rounded-bl-[30px] rounded-br-[30px] border-b-8 border-sky-800">
                     <div className="flex items-center gap-2 mx-auto">
                         <h2 className="text-base font-extrabold lg:text-2xl text-indigo-950">Content Info</h2>
@@ -62,36 +59,35 @@ const CreateNewSection = () => {
                 </div>
 
 
-                <div className="   px-4 py-4 ">
-                        <div>
-                            <label htmlFor="title" className={labelClasses}>
-                                Title:
-                            </label>
-                            <input
-                                type="text"
-                                ref={titleRef}
-                                id="title"
-                                name="title"
-                                className={`${inputClasses} lg:font-bold`}
-                            />
+                <div className="px-2 md:px-4 py-4 md:mx-10 ">
+                    <form>
+                        <label className="text-black text-xl font-semibold font-['Open Sans']">Title:</label>
+                        <div className="flex items-center md:ml-4 mt-3">
+                            <div className="flex justify-center items-center px-5 py-2.5 text-sm font-medium text-center bg-sky-800  text-white">
+                                Lecture
+                            </div>
+                            <div className="relative w-full">
+                                <input
+                                    type="text"
+                                    ref={titleRef}
+                                    id="title"
+                                    className=" px-4 py-2 bg-zinc-100  h-full  rounded-sm w-full  outline-none  border border-black border-opacity-80"
+                                    placeholder="Name anything"
+                                />
+                            </div>
                         </div>
-                    <div className="flex items-center gap-4 lg:items-end">
+                    </form>
+
+                    <div className="flex justify-end mt-12">
                         <button
-                            onClick={handleSavingCourseInformation}
-                            className={`px-6 py-2 text-lg font-semibold tracking-tight text-white duration-300 rounded-lg hover:animate-pulse ${missingError
-                                ? "bg-red-900 hover:bg-red-800"
-                                : "bg-stone-900 hover:bg-stone-800"
-                                }`}
+                            // onClick={handleSavingCourseInformation}
+                            className="px-6 py-2 opacity-90 text-lg font-bold font-['Outfit'] tracking-wide text-white duration-300 hover:animate-pulse bg-sky-800 rounded-[25px] shadow"
                         >
-                            Save
+                            Next
                         </button>
-                        {missingError && (
-                            <span className="text-sm italic text-red-700 capitalize">
-                                incomplete information
-                            </span>
-                        )}
                     </div>
                 </div>
+
             </div>
         </>
     );
