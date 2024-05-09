01fa29d3900e023ab6ef51442e9075d222557473
FrontEndDevo <mylinkacco@gmail.com>
2024-05-10 02:48:24 +0300

    Add public_course_with_subject action to fetch courses based on subject

--- src/redux/actions/courses-methods.js
+++ src/redux/actions/courses-methods.js
@@ -1,14 +1,31 @@
 import axios from "axios";
-import { setPublicCourses } from "../slices/courses/courses-slice";
-import { setPublicCoursesError } from "../slices/courses/courses-errors";
+import {
+  setCoursesDependOnSubject,
+  setPublicCourses,
+} from "../slices/courses/courses-slice";
+import {
+  setCoursesDependOnSubjectError,
+  setPublicCoursesError,
+} from "../slices/courses/courses-errors";
 
 export const public_courses = async (dispatch) => {
   try {
     const res = await axios.get(
       import.meta.env.VITE_API_URL + "/api/public/courses/"
     );
-    dispatch(setPublicCourses(res.data.results));
+    dispatch(setPublicCourses(res.data));
   } catch (err) {
     dispatch(setPublicCoursesError());
+  }
+};
+
+export const public_course_with_subject = async (dispatch, subject) => {
+  try {
+    const res = await axios.get(
+      import.meta.env.VITE_API_URL + "/api/public/subject/" + subject
+    );
+    dispatch(setCoursesDependOnSubject(res.data));
+  } catch (err) {
+    dispatch(setCoursesDependOnSubjectError());
   }
 };
