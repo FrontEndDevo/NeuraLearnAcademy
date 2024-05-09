f5f32f1e4fb5f492bc6b70cafa8a33302b705548
karim Abdelazim <ka1465@fayoum.edu.eg>
2024-05-09 14:50:22 +0300

    create header of course content for admin.

--- src/components/Instructor/CoureseContent/CoursesContent.jsx
+++ src/components/Instructor/CoureseContent/CoursesContent.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGraduationCap } from '@fortawesome/free-solid-svg-icons';
 const CoursesContent = () => {
     return (
         <>
+            <header className="p-10 bg-[#004682] text-white">
+                <h1 className="text-2xl font-bold">Course Machine learning</h1>
+                <p>Course Machine learning this the best course.</p>
+                <p className="text-sm mt-4">
+                    <span>0 Students</span>
+                </p>
+                <p className="text-sm">
+                    <span>Last updated </span>
+                    <span>2/7/2024</span>
+                </p>
+            </header>
             
         </>
     )
