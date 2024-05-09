3f2385467a8026cf40bd5caedfd25b7752b6b706
karim Abdelazim <ka1465@fayoum.edu.eg>
2024-05-09 23:06:49 +0300

    replace icon from font awsome with icon from svg.

--- src/components/Instructor/CoureseContent/CoursesContent.jsx
+++ src/components/Instructor/CoureseContent/CoursesContent.jsx
@@ -27,11 +27,14 @@ const SectionHeader = ({ sectionTitle, onDelete, onEdit }) => {
         <button>
           <FontAwesomeIcon onClick={onDelete} icon={faTrash} />
         </button>
-        <button>
-          <FontAwesomeIcon icon={faFolderOpen} />
-        </button>
         <button onClick={onEdit}>
           <FontAwesomeIcon icon={faPenToSquare} />
+        </button>
+        <button>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 10.5v6m3-3H9m4.06-7.19-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z" />
+          </svg>
+
         </button>
       </div>
     </div>
