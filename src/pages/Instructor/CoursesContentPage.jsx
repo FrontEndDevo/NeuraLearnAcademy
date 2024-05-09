d62096d69e09ebf9b7699eb8ce183441052805ba
karim Abdelazim <ka1465@fayoum.edu.eg>
2024-05-10 02:36:25 +0300

    add condtion if modal == section info show it.

--- src/pages/Instructor/CoursesContentPage.jsx
+++ src/pages/Instructor/CoursesContentPage.jsx
@@ -2,9 +2,13 @@ import { createPortal } from "react-dom";
 import { useSelector } from "react-redux";
 import CoursesContent from "../../components/Instructor/CoureseContent/CoursesContent";
 import CreateNewSection from "../../components/Instructor/CreateSections/CreateNewSection";
+import SectionInfo from "../../components/Instructor/CreateSections/SectionInfo";
 
 const newInstructorCourseId = document.getElementById(
     "instructor__course__content"
+);
+const sectionInfo = document.getElementById(
+    "section__info"
 );
 const CoursesContentPage = () => {
     const modalName = useSelector((state) => state.openClose.modalName);
@@ -13,6 +17,8 @@ const CoursesContentPage = () => {
             <CoursesContent />
             {modalName === "createNewSection" &&
                 createPortal(<CreateNewSection />, newInstructorCourseId)}
+            {modalName === "sectioninfo" &&
+                createPortal(<SectionInfo />, sectionInfo)}
         </>
     );
 };
