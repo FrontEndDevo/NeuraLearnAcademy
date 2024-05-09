bcaa83b2392c554b3a89cd8d416df578c2f652cf
karim Abdelazim <ka1465@fayoum.edu.eg>
2024-05-09 14:58:07 +0300

    import some of icons from font awsome.

--- src/components/Instructor/CoureseContent/CoursesContent.jsx
+++ src/components/Instructor/CoureseContent/CoursesContent.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGraduationCap } from '@fortawesome/free-solid-svg-icons';
+import { faGraduationCap, faClock } from '@fortawesome/free-solid-svg-icons';
 const CoursesContent = () => {
     return (
         <>
@@ -8,14 +8,14 @@ const CoursesContent = () => {
                 <h1 className="text-2xl font-bold">Course Machine learning</h1>
                 <p>Course Machine learning this the best course.</p>
                 <p className="text-sm mt-4">
-                    <span>0 Students</span>
+                    <span>  <FontAwesomeIcon icon={faGraduationCap} /> 0 Students</span>
                 </p>
                 <p className="text-sm">
-                    <span>Last updated </span>
+                    <span><FontAwesomeIcon icon={faClock} /> Last updated </span>
                     <span>2/7/2024</span>
                 </p>
             </header>
-            
+
         </>
     )
 }
