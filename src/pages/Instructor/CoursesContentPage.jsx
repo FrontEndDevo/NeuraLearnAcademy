0efc3aeed47a7745d145ee0687aad96068df0d11
karim Abdelazim <ka1465@fayoum.edu.eg>
2024-06-11 03:03:29 +0300

    edit the courses content.

--- src/pages/Instructor/CoursesContentPage.jsx
+++ src/pages/Instructor/CoursesContentPage.jsx
@@ -1,34 +1,21 @@
 import { createPortal } from "react-dom";
 import { useSelector } from "react-redux";
 import CoursesContent from "../../components/Instructor/CoureseContent/CoursesContent";
-import CreateNewSection from "../../components/Instructor/CreateSections/CreateNewSection";
 import SectionInfo from "../../components/Instructor/CreateSections/SectionInfo";
-import AddVideo from "../../components/Instructor/CreateSections/AddVideo";
-import AddPhoto from "../../components/Instructor/CreateSections/AddPhoto";
-import AddText from "../../components/Instructor/CreateSections/AddText";
-import AddFile from "../../components/Instructor/CreateSections/AddFile";
-import DeleteSection from "../../components/Instructor/CreateSections/DeleteSection";
 
-const newInstructorCourseId = document.getElementById(
-    "instructor__course__content"
-);
+import DeleteSection from "../../components/Instructor/CreateSections/DeleteSection";
+import CreateNewSection from './../../components/Instructor/CreateSections/CreateNewSection';
+
+
 const sectionInfo = document.getElementById(
     "section__info"
 );
-const addVideo = document.getElementById(
-    "addvideo__modal"
-);
-const addphoto = document.getElementById(
-    "addphoto__modal"
-);
-const addtext = document.getElementById(
-    "addtext__modal"
-);
-const addfile = document.getElementById(
-    "addfile__modal"
-);
+
 const deletesection = document.getElementById(
     "deletesection__modal"
+);
+const newInstructorCourseId = document.getElementById(
+    "instructor__course__content"
 );
 const CoursesContentPage = () => {
     const modalName = useSelector((state) => state.openClose.modalName);
@@ -39,15 +26,7 @@ const CoursesContentPage = () => {
                 createPortal(<CreateNewSection />, newInstructorCourseId)}
             {modalName === "sectioninfo" &&
                 createPortal(<SectionInfo />, sectionInfo)}
-            {modalName === "addvideo__modal" &&
-                createPortal(<AddVideo />, addVideo)}
-            
-            {modalName === "addphoto__modal" &&
-                createPortal(<AddPhoto />, addphoto)}
-            {modalName === "addtext__modal" &&
-                createPortal(<AddText />, addtext)}
-            {modalName === "addfile__modal" &&
-                createPortal(<AddFile />, addfile)}
+      
             {modalName === "deletesection__modal" &&
                 createPortal(<DeleteSection />, deletesection)}
         </>
