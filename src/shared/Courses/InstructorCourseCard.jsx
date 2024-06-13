384975a8abd76bfe406fb2c43aeed78361adc2b4
BasemYahia402 <basemyahia70@gmail.com>
2024-06-13 21:11:09 +0300

    Make cursor pointer on card and send slug of course

--- src/shared/Courses/InstructorCourseCard.jsx
+++ src/shared/Courses/InstructorCourseCard.jsx
@@ -21,6 +21,7 @@ const InstructorCourseCard = ({
   quizzes,
   students,
   price,
+  slug,
 }) => {
   const courseTitle = title.length <= 50 ? title : title.slice(0, 50) + "...";
 
@@ -52,88 +53,90 @@ const InstructorCourseCard = ({
   ));
 
   return (
-    <li className="duration-300 border shadow-lg rounded-3xl hover:shadow-innerwhite">
-      <img
-        src={image}
-        alt={title}
-        className="w-full h-60 rounded-t-3xl"
-        loading="lazy"
-      />
+    <Link to={`/CoursesContentPage/${slug}`}>
+      <li className="duration-300 border shadow-lg rounded-3xl hover:shadow-innerwhite">
+        <img
+          src={image}
+          alt={title}
+          className="w-full h-60 rounded-t-3xl"
+          loading="lazy"
+        />
 
-      <div className="px-4 py-2">
-        <div className="flex items-center justify-between">
-          <h3 className="my-4 text-base font-semibold leading-6 tracking-wide lg:text-xl text-gray-color-700">
-            {courseTitle}
-          </h3>
-          <div className="flex gap-1 p-2 cursor-pointer lg:hover:animate-pulse">
-            {Array.from({ length: 3 }, (_, i) => (
-              <span
-                key={i}
-                className="w-2 h-2 rounded-full bg-gray-color-500"
-              />
-            ))}
+        <div className="px-4 py-2">
+          <div className="flex items-center justify-between">
+            <h3 className="my-4 text-base font-semibold leading-6 tracking-wide lg:text-xl text-gray-color-700">
+              {courseTitle}
+            </h3>
+            <div className="flex gap-1 p-2 cursor-pointer lg:hover:animate-pulse">
+              {Array.from({ length: 3 }, (_, i) => (
+                <span
+                  key={i}
+                  className="w-2 h-2 rounded-full bg-gray-color-500"
+                />
+              ))}
+            </div>
+          </div>
+
+          <div className="grid grid-cols-3 gap-4">{courseProperties}</div>
+
+          <h6 className="px-3 py-1 text-[10px] my-3 lg:text-base capitalize font-bold text-white rounded-full bg-secondary-900 w-fit">
+            {subject}
+          </h6>
+
+          <div className="flex items-center justify-between">
+            <div className="flex items-center gap-1 text-sm tracking-tight">
+              <FontAwesomeIcon icon={faUserGraduate} />
+              <p className="font-semibold">{numberOfStudents}</p>
+              <span className="text-gray-400">({students}) Students</span>
+            </div>
+            <p className="text-base font-semibold">${price}</p>
           </div>
         </div>
 
-        <div className="grid grid-cols-3 gap-4">{courseProperties}</div>
-
-        <h6 className="px-3 py-1 text-[10px] my-3 lg:text-base capitalize font-bold text-white rounded-full bg-secondary-900 w-fit">
-          {subject}
-        </h6>
-
-        <div className="flex items-center justify-between">
-          <div className="flex items-center gap-1 text-sm tracking-tight">
-            <FontAwesomeIcon icon={faUserGraduate} />
-            <p className="font-semibold">{numberOfStudents}</p>
-            <span className="text-gray-400">({students}) Students</span>
+        <div className="flex items-center justify-between mx-2 mt-1 mb-2 md:mt-4">
+          <div className="relative flex items-center gap-2">
+            <FontAwesomeIcon
+              icon={faPenToSquare}
+              className="absolute top-0 left-0 p-2 bg-white rounded-full md:p-3 text-primary-500"
+            />
+            <button
+              onClick={handleOpenCreateCourse}
+              className="p-2 pl-12 text-sm font-semibold text-white duration-200 rounded-full md:text-base bg-primary-500 hover:bg-primary-700"
+            >
+              Edit
+            </button>
           </div>
-          <p className="text-base font-semibold">${price}</p>
-        </div>
-      </div>
 
-      <div className="flex items-center justify-between mx-2 mt-1 mb-2 md:mt-4">
-        <div className="relative flex items-center gap-2">
-          <FontAwesomeIcon
-            icon={faPenToSquare}
-            className="absolute top-0 left-0 p-2 bg-white rounded-full md:p-3 text-primary-500"
-          />
-          <button
-            onClick={handleOpenCreateCourse}
-            className="p-2 pl-12 text-sm font-semibold text-white duration-200 rounded-full md:text-base bg-primary-500 hover:bg-primary-700"
-          >
-            Edit
-          </button>
+          <div className="relative flex items-center gap-2">
+            <FontAwesomeIcon
+              icon={faListUl}
+              className="absolute top-0 left-0 p-2 bg-white rounded-full md:p-3 text-primary-500"
+            />
+            <Link
+              to={formattedTitle}
+              className="p-2 pl-12 text-sm font-semibold text-white duration-200 rounded-full md:text-base bg-primary-500 hover:bg-primary-700"
+            >
+              Edit Sections
+            </Link>
+          </div>
         </div>
 
-        <div className="relative flex items-center gap-2">
-          <FontAwesomeIcon
-            icon={faListUl}
-            className="absolute top-0 left-0 p-2 bg-white rounded-full md:p-3 text-primary-500"
-          />
-          <Link
-            to={formattedTitle}
-            className="p-2 pl-12 text-sm font-semibold text-white duration-200 rounded-full md:text-base bg-primary-500 hover:bg-primary-700"
-          >
-            Edit Sections
-          </Link>
+        <div className="flex flex-wrap items-center justify-between gap-4 mx-2 mt-2 mb-4 md:mt-4">
+          <div className="relative flex items-center gap-2">
+            <FontAwesomeIcon
+              icon={faPenToSquare}
+              className="absolute top-0 left-0 p-2 bg-white rounded-full md:p-3 text-primary-500"
+            />
+            <button
+              onClick={handleOpenCreateCourse}
+              className="p-2 pl-12 text-sm font-semibold text-white duration-200 rounded-full md:text-base bg-primary-500 hover:bg-primary-700"
+            >
+              Edit
+            </button>
+          </div>
         </div>
-      </div>
-
-      <div className="flex flex-wrap items-center justify-between gap-4 mx-2 mt-2 mb-4 md:mt-4">
-        <div className="relative flex items-center gap-2">
-          <FontAwesomeIcon
-            icon={faPenToSquare}
-            className="absolute top-0 left-0 p-2 bg-white rounded-full md:p-3 text-primary-500"
-          />
-          <button
-            onClick={handleOpenCreateCourse}
-            className="p-2 pl-12 text-sm font-semibold text-white duration-200 rounded-full md:text-base bg-primary-500 hover:bg-primary-700"
-          >
-            Edit
-          </button>
-        </div>
-      </div>
-    </li>
+      </li>
+    </Link>
   );
 };
 
