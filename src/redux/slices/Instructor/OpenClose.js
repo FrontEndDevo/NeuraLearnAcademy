d89059bb8b117d890e1dcc525ee96f0e516c9377
BasemYahia402 <basemyahia70@gmail.com>
2024-06-14 20:54:12 +0300

    Add lecture to initialState of openCloseSlice

--- src/redux/slices/Instructor/OpenClose.js
+++ src/redux/slices/Instructor/OpenClose.js
@@ -2,16 +2,23 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const openCloseSlice = createSlice({
   name: "openClose",
-  initialState: { modalName: null, slug: null, courseDetails: null },
+  initialState: {
+    modalName: null,
+    slug: null,
+    lecture: null,
+    courseDetails: null,
+  },
   reducers: {
     openModal: (state, action) => {
       state.modalName = action.payload.modalName;
       state.slug = action.payload.slug;
+      state.lecture = action.payload.lecture;
       state.courseDetails = action.payload.course || null;
     },
     closeModal: (state) => {
       state.modalName = null;
       state.slug = null;
+      state.lecture = null;
       state.courseDetails = null;
     },
   },
