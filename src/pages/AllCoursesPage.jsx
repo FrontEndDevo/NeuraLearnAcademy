1e19d6218c344e53bb78be1f10a89758f0cfd5b9
FrontEndDevo <mylinkacco@gmail.com>
2024-06-20 16:50:25 +0300

    Replace the temp Categories with the fetched one in all-courses page

--- src/pages/AllCoursesPage.jsx
+++ src/pages/AllCoursesPage.jsx
@@ -1,27 +1,14 @@
+import { useSelector } from "react-redux";
 import AllCourses from "../components/AllCourses/AllCourses";
 import CoursesCategories from "../components/AllCourses/CoursesCategories";
 
-export const allCategories = [
-  "all",
-  "programming",
-  "development",
-  "data science",
-  "business",
-  "marketing",
-  "sales",
-  "drawing",
-  "photography",
-  "category_1",
-  "category_2",
-  "category_3",
-  "category_4",
-];
-
 const AllCoursesPage = () => {
+  const categories = useSelector((state) => state.courses.subjectCourses);
+
   return (
     <section className="grid grid-cols-1 gap-2 m-4 my-10 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 xl:m-10">
       <div>
-        <CoursesCategories categories={allCategories} />
+        <CoursesCategories categories={categories} />
       </div>
 
       <div className="col-span-1 md:col-span-2 lg:col-span-3 xl:col-span-4">
