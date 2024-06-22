0c33f5c16776b3b0b3ae5c48597a10fb5e03a1ed
BasemYahia402 <basemyahia70@gmail.com>
2024-06-23 00:06:35 +0300

    Call Layout component and Delete unused code

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import Layout from "./pages/Layout";
 import Homepage from "./pages/Homepage";
 import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
@@ -22,54 +23,60 @@ import CoursesContentPage from "./pages/Instructor/CoursesContentPage";
 import RequireAuth from "./components/Registration/RequireAuth/RequireAuth";
 import QuestionGenerationPage from "./pages/QuestionGenerationPage";
 import UserPage from "./pages/Users/UserPage";
-import Spinner from "./shared/popup/Spinner";
-import { createPortal } from "react-dom";
-import { useSelector } from "react-redux";
-
-const loadingSpinnerId = document.getElementById("loading__spinner");
 
 const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
-        <Route
-          path="/password/reset/confirm/:uid/:token"
-          element={<ResetPasswordConfirm />}
-        />
-        <Route path="/activate/:uid/:token" element={<Activation />} />
-        <Route path="/*" element={<PageNotFound />} />
-        <Route element={<RequireAuth />}>
-          <Route path="/my-learnings" element={<MyLearningsPage />} />
-          <Route path="/cart" element={<ShoppingCartPage />} />
-          <Route path="/all-courses" element={<AllCoursesPage />} />
-          <Route path="/instructor" element={<InstructorPage />} />
-          <Route path="/profile" element={<ProfilePage />}>
-            <Route path="profileInfo" element={<ProfileInfoPage />} />
-            <Route
-              path="password&security"
-              element={<ProfilePasswordSecurityPage />}
-            />
-            <Route path="privacy" element={<ProfilePrivacyPage />} />
-            <Route path="Notification" element={<ProfileNotificationPage />} />
-            <Route path="close-account" element={<ProfileCloseAcountPage />} />
-          </Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Homepage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="signup" element={<SignUpPage />} />
+          <Route path="reset-password" element={<ResetPassword />} />
+          <Route
+            path="password/reset/confirm/:uid/:token"
+            element={<ResetPasswordConfirm />}
+          />
+          <Route path="activate/:uid/:token" element={<Activation />} />
+          <Route path="*" element={<PageNotFound />} />
+          <Route element={<RequireAuth />}>
+            <Route path="my-learnings" element={<MyLearningsPage />} />
+            <Route path="cart" element={<ShoppingCartPage />} />
+            <Route path="all-courses" element={<AllCoursesPage />} />
+            <Route path="instructor" element={<InstructorPage />} />
+            <Route path="profile" element={<ProfilePage />}>
+              <Route path="profileInfo" element={<ProfileInfoPage />} />
+              <Route
+                path="password&security"
+                element={<ProfilePasswordSecurityPage />}
+              />
+              <Route path="privacy" element={<ProfilePrivacyPage />} />
+              <Route
+                path="Notification"
+                element={<ProfileNotificationPage />}
+              />
+              <Route
+                path="close-account"
+                element={<ProfileCloseAcountPage />}
+              />
+            </Route>
 
-          <Route path="summarizer" element={<Summarizers />} />
-          <Route
-            path="questionqeneration"
-            element={<QuestionGenerationPage />}
-          />
-          <Route path="ChatBot" element={<ChatBot />} />
-          <Route path="CoursesContentPage/:slug" element={<CoursesContentPage />} />
-          <Route path="UserContentPage/:slug" element={<UserPage />} />
-          <Route
-            path="CoursesContentPage/:slug"
-            element={<CoursesContentPage />}
-          />
+            <Route path="summarizer" element={<Summarizers />} />
+            <Route
+              path="questionqeneration"
+              element={<QuestionGenerationPage />}
+            />
+            <Route path="ChatBot" element={<ChatBot />} />
+            <Route
+              path="CoursesContentPage/:slug"
+              element={<CoursesContentPage />}
+            />
+            <Route path="UserContentPage/:slug" element={<UserPage />} />
+            <Route
+              path="CoursesContentPage/:slug"
+              element={<CoursesContentPage />}
+            />
+          </Route>
         </Route>
       </Routes>
     </>
