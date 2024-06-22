83926ddbc1c101894fc698753eb2a523bfae3821
FrontEndDevo <mylinkacco@gmail.com>
2024-06-22 15:21:16 +0300

    set the basic styles for each element in the navbar

--- src/common/Navbar/NavSearch.jsx
+++ src/common/Navbar/NavSearch.jsx
@@ -1,14 +1,21 @@
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 const NavSearch = () => {
   return (
-    <div>
+    <div className="relative flex items-stretch">
+      <FontAwesomeIcon
+        icon={faMagnifyingGlass}
+        className="absolute z-10 text-gray-400 left-2 top-1/3"
+      />
       <input
         type="text"
         name="search"
         id="search"
         placeholder="Search for anything..."
-        className="w-full px-1 md:px-4 font-semibold py-3 text-xs border border-r-0 outline-none tracking-wide rounded-[3px] border-zinc-600 caret-gray-color-700"
+        className="px-1 md:px-4 md:pl-8 font-semibold py-3 text-xs outline-none tracking-wide rounded-[3px] rounded-r-none border border-neutral-800 text-neutral-800 caret-neutral-800"
       />
-      <button className="bg-blue-700 text-white px-4 py-3 rounded-r-[3px]">
+      <button className="bg-neutral-800 duration-200 hover:bg-neutral-900 uppercase text-white px-4 py-3 rounded-r-[3px]">
         Search
       </button>
       <div>{/* Dropdown menu for search results */}</div>
--- src/common/Navbar/Navbar.jsx
+++ src/common/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import nlaLogo from "../../assets/images/LoginSigin/logo.png";
 import NavSearch from "./NavSearch";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -20,31 +20,49 @@ const navLinks = [
 
 const Navbar = () => {
   const navbarLinks = navLinks.map((item) => (
-    <li key={item.id}>
-      <Link to={item.link}>{item.title}</Link>
-    </li>
+    <Link
+      to={item.link}
+      key={item.id}
+      className="p-2 font-semibold capitalize duration-200 rounded-lg hover:bg-neutral-800 hover:text-white"
+    >
+      {item.title}
+    </Link>
   ));
   return (
-    <nav>
-      <div>
-        <div>
-          <img src={nlaLogo} alt="NeuraLearnAcademy" />
-          <h1>NeuraLearnAcademy</h1>
+    <nav className="fixed top-0 left-0 z-50 flex items-center justify-between w-screen px-6 py-4 bg-white shadow-lg">
+      <div className="flex items-center gap-4">
+        <div className="flex items-center gap-2">
+          <img src={nlaLogo} alt="NeuraLearnAcademy" className="w-10 h-10" />
+          <h1 className="text-xl font-bold">NeuraLearnAcademy</h1>
         </div>
         <NavSearch />
       </div>
 
-      <div>
-        <ul>{navbarLinks}</ul>
-        <div>
-          <Link to="/login">Log in</Link>
-          <Link to="/signup">Sign Up</Link>
+      <div className="flex items-center gap-10">
+        <div className="flex items-center gap-10">{navbarLinks}</div>
+        <div className="flex items-center gap-4 font-semibold uppercase">
+          <Link
+            to="/login"
+            className="px-4 py-2 duration-200 border rounded-lg border-neutral-800 hover:bg-neutral-200"
+          >
+            Log in
+          </Link>
+          <Link
+            to="/signup"
+            className="px-4 py-2 text-white duration-200 rounded-lg bg-neutral-800 hover:bg-neutral-900"
+          >
+            Sign Up
+          </Link>
         </div>
 
         <div>
-          <div>
-            <img src={defaultUserPicture} alt="My profile" />
-            <FontAwesomeIcon icon={faCaretDown} />
+          <div className="flex items-center gap-1 pr-1 duration-200 rounded-full cursor-pointer hover:bg-gray-200">
+            <img
+              src={defaultUserPicture}
+              alt="My profile"
+              className="w-8 h-8 p-1 border rounded-full border-neutral-800"
+            />
+            <FontAwesomeIcon icon={faCaretDown} className="w-2 h-2" />
           </div>
           <div>{/* Dropdown menu for profile options */}</div>
         </div>
