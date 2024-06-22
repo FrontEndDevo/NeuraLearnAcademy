efc8812a588ea827575629a2b3acc72e74e77c45
FrontEndDevo <mylinkacco@gmail.com>
2024-06-23 01:51:07 +0300

    Render some elements conditionally depending on if the user is authenticated or not

--- src/common/Navbar/NavSearch.jsx
+++ src/common/Navbar/NavSearch.jsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const NavSearch = () => {
   return (
-    <div className="relative flex items-stretch">
+    <div className="relative flex items-stretch w-full">
       <FontAwesomeIcon
         icon={faMagnifyingGlass}
         className="absolute z-10 text-gray-400 left-2 top-1/3"
@@ -13,7 +13,7 @@ const NavSearch = () => {
         name="search"
         id="search"
         placeholder="Search for anything..."
-        className="px-1 md:px-4 md:pl-8 font-semibold py-3 text-xs outline-none tracking-wide rounded-[3px] rounded-r-none border border-neutral-800 text-neutral-800 caret-neutral-800"
+        className="w-full px-1 md:px-4 md:pl-8 font-semibold py-3 text-xs outline-none tracking-wide rounded-[3px] rounded-r-none border border-neutral-800 text-neutral-800 caret-neutral-800"
       />
       <button className="bg-neutral-800 duration-200 hover:bg-neutral-900 uppercase text-white px-4 py-3 rounded-r-[3px]">
         Search
--- src/common/Navbar/Navbar.jsx
+++ src/common/Navbar/Navbar.jsx
@@ -6,81 +6,96 @@ import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import defaultUserPicture from "../../assets/images/profile/unknown_user.webp";
 import ProfileDropdown from "./ProfileDropdown";
 import { useState } from "react";
-
-const navLinks = [
-  {
-    id: 1,
-    title: "Home",
-    link: "/",
-  },
-  {
-    id: 2,
-    title: "my learnings",
-    link: "/my-learnings",
-  },
-];
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
   const [showDropdown, setShowDropdown] = useState(false);
 
+  const userAuth = useSelector((state) => state.userAuth);
+  const { isAuthenticated, user } = userAuth;
   const handleTogglingProfileDropdown = () => {
     setShowDropdown((prevState) => !prevState);
   };
 
-  const navbarLinks = navLinks.map((item) => (
-    <Link
-      to={item.link}
-      key={item.id}
-      className="p-2 font-semibold capitalize duration-200 rounded-lg hover:bg-neutral-800 hover:text-white"
-    >
-      {item.title}
-    </Link>
-  ));
-
   return (
     <nav className="fixed top-0 z-50 flex items-center justify-between w-screen px-6 py-4 pr-20 bg-white shadow-lg">
-      <div className="flex items-center gap-4">
-        <div className="flex items-center gap-2">
+      <div className="flex items-center w-1/2 gap-14">
+        <Link to="/" className="flex items-center gap-2">
           <img src={nlaLogo} alt="NeuraLearnAcademy" className="w-10 h-10" />
           <h1 className="text-xl font-bold">NeuraLearnAcademy</h1>
-        </div>
+        </Link>
         <NavSearch />
       </div>
 
       <div className="flex items-center gap-10">
-        <ul className="flex items-center gap-10">{navbarLinks}</ul>
-        <div className="flex items-center gap-4 font-semibold uppercase">
+        <div className="flex items-center gap-10">
           <Link
-            to="/login"
-            className="px-4 py-2 duration-200 border rounded-lg border-neutral-800 hover:bg-neutral-200"
+            to="/"
+            className="p-2 font-semibold capitalize duration-200 rounded-lg hover:bg-neutral-800 hover:text-white"
           >
-            Log in
+            home
           </Link>
+
+          {isAuthenticated && (
+            <Link
+              to="/my-learnings"
+              className="p-2 font-semibold capitalize duration-200 rounded-lg hover:bg-neutral-800 hover:text-white"
+            >
+              my learnings
+            </Link>
+          )}
+
           <Link
-            to="/signup"
-            className="px-4 py-2 text-white duration-200 rounded-lg bg-neutral-800 hover:bg-neutral-900"
+            to="/contact-us"
+            className="p-2 font-semibold capitalize duration-200 rounded-lg hover:bg-neutral-800 hover:text-white"
           >
-            Sign Up
+            contact us
+          </Link>
+
+          <Link
+            to="/about-us"
+            className="p-2 font-semibold capitalize duration-200 rounded-lg hover:bg-neutral-800 hover:text-white"
+          >
+            about us
           </Link>
         </div>
 
-        <div
-          onClick={handleTogglingProfileDropdown}
-          className="relative flex items-center gap-1 pr-1 duration-200 rounded-full cursor-pointer hover:bg-gray-200"
-        >
-          <img
-            src={defaultUserPicture}
-            alt="My profile"
-            className="w-8 h-8 p-1 border rounded-full border-neutral-800"
-          />
-          <FontAwesomeIcon
-            icon={faCaretDown}
-            className={`w-2 h-2 duration-200 ${
-              showDropdown ? "transform rotate-180" : ""
-            }`}
-          />
-          <ProfileDropdown showOptions={showDropdown} />
-        </div>
+        {!isAuthenticated && (
+          <div className="flex items-center gap-4 font-semibold uppercase">
+            <Link
+              to="/login"
+              className="px-4 py-2 duration-200 border rounded-lg border-neutral-800 hover:bg-neutral-200"
+            >
+              Log in
+            </Link>
+            <Link
+              to="/signup"
+              className="px-4 py-2 text-white duration-200 rounded-lg bg-neutral-800 hover:bg-neutral-900"
+            >
+              Sign Up
+            </Link>
+          </div>
+        )}
+
+        {isAuthenticated && (
+          <div
+            onClick={handleTogglingProfileDropdown}
+            className="relative flex items-center gap-1 pr-1 duration-200 rounded-full cursor-pointer hover:bg-gray-200"
+          >
+            <img
+              src={defaultUserPicture}
+              alt="My profile"
+              className="w-8 h-8 p-1 border rounded-full border-neutral-800"
+            />
+            <FontAwesomeIcon
+              icon={faCaretDown}
+              className={`w-2 h-2 duration-200 ${
+                showDropdown ? "transform rotate-180" : ""
+              }`}
+            />
+            <ProfileDropdown showOptions={showDropdown} />
+          </div>
+        )}
       </div>
     </nav>
   );
