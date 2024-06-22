14edf02f543deb64f080ac1bd64960f04569a3c2
FrontEndDevo <mylinkacco@gmail.com>
2024-06-22 17:21:00 +0300

    Create the ProfileDropdown component for navbar

--- src/common/Navbar/Navbar.jsx
+++ src/common/Navbar/Navbar.jsx
@@ -1,9 +1,11 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import nlaLogo from "../../assets/images/LoginSigin/logo.png";
 import NavSearch from "./NavSearch";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import defaultUserPicture from "../../assets/images/profile/unknown_user.webp";
+import ProfileDropdown from "./ProfileDropdown";
+import { useState } from "react";
 
 const navLinks = [
   {
@@ -19,6 +21,12 @@ const navLinks = [
 ];
 
 const Navbar = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  const handleTogglingProfileDropdown = () => {
+    setShowDropdown((prevState) => !prevState);
+  };
+
   const navbarLinks = navLinks.map((item) => (
     <Link
       to={item.link}
@@ -28,8 +36,9 @@ const Navbar = () => {
       {item.title}
     </Link>
   ));
+
   return (
-    <nav className="fixed top-0 left-0 z-50 flex items-center justify-between w-screen px-6 py-4 bg-white shadow-lg">
+    <nav className="fixed top-0 z-50 flex items-center justify-between w-screen px-6 py-4 pr-20 bg-white shadow-lg">
       <div className="flex items-center gap-4">
         <div className="flex items-center gap-2">
           <img src={nlaLogo} alt="NeuraLearnAcademy" className="w-10 h-10" />
@@ -39,7 +48,7 @@ const Navbar = () => {
       </div>
 
       <div className="flex items-center gap-10">
-        <div className="flex items-center gap-10">{navbarLinks}</div>
+        <ul className="flex items-center gap-10">{navbarLinks}</ul>
         <div className="flex items-center gap-4 font-semibold uppercase">
           <Link
             to="/login"
@@ -55,16 +64,22 @@ const Navbar = () => {
           </Link>
         </div>
 
-        <div>
-          <div className="flex items-center gap-1 pr-1 duration-200 rounded-full cursor-pointer hover:bg-gray-200">
-            <img
-              src={defaultUserPicture}
-              alt="My profile"
-              className="w-8 h-8 p-1 border rounded-full border-neutral-800"
-            />
-            <FontAwesomeIcon icon={faCaretDown} className="w-2 h-2" />
-          </div>
-          <div>{/* Dropdown menu for profile options */}</div>
+        <div
+          onClick={handleTogglingProfileDropdown}
+          className="relative flex items-center gap-1 pr-1 duration-200 rounded-full cursor-pointer hover:bg-gray-200"
+        >
+          <img
+            src={defaultUserPicture}
+            alt="My profile"
+            className="w-8 h-8 p-1 border rounded-full border-neutral-800"
+          />
+          <FontAwesomeIcon
+            icon={faCaretDown}
+            className={`w-2 h-2 duration-200 ${
+              showDropdown ? "transform rotate-180" : ""
+            }`}
+          />
+          <ProfileDropdown showOptions={showDropdown} />
         </div>
       </div>
     </nav>
---
+++ src/common/Navbar/ProfileDropdown.jsx
@@ -0,0 +1,81 @@
+import PropsTypes from "prop-types";
+import {
+  faBell,
+  faIdCard,
+  faLock,
+  faPowerOff,
+  faShield,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import { logout } from "../../redux/actions/auth-methods";
+import { useDispatch } from "react-redux";
+
+const profileOptions = [
+  {
+    id: 1,
+    title: "profile info",
+    link: "/profile/profileInfo",
+    icon: faIdCard,
+  },
+  {
+    id: 2,
+    title: "password & security",
+    link: "/profile/password&security",
+    icon: faLock,
+  },
+  {
+    id: 3,
+    title: "privacy",
+    link: "/profile/privacy",
+    icon: faShield,
+  },
+  {
+    id: 4,
+    title: "notification",
+    link: "/profile/notification",
+    icon: faBell,
+  },
+];
+
+const ProfileDropdown = ({ showOptions }) => {
+  const dispatch = useDispatch();
+
+  const handleSignOut = () => {
+    logout(dispatch);
+  };
+
+  return (
+    <div
+      className={`absolute top-full right-0 gap-2 z-50 duration-200 border-2 bg-white shadow-lg rounded-md ${
+        showOptions
+          ? "opacity-100 translate-y-0"
+          : "opacity-0 -translate-y-5 pointer-events-none"
+      }`}
+    >
+      {profileOptions.map((opt) => (
+        <Link
+          key={opt.id}
+          to={opt.link}
+          className="flex items-center gap-2 px-4 py-3 pr-12 text-sm font-semibold capitalize duration-200 cursor-pointer whitespace-nowrap hover:bg-indigo-500 hover:text-white"
+        >
+          <FontAwesomeIcon icon={opt.icon} />
+          {opt.title}
+        </Link>
+      ))}
+      <div
+        onClick={handleSignOut}
+        className="flex items-center gap-2 px-4 py-3 pr-12 text-sm font-semibold duration-200 cursor-pointer hover:bg-indigo-500 hover:text-white"
+      >
+        <FontAwesomeIcon icon={faPowerOff} />
+        <button className="capitalize">sign out</button>
+      </div>
+    </div>
+  );
+};
+
+ProfileDropdown.propTypes = {
+  showOptions: PropsTypes.bool.isRequired,
+};
+
+export default ProfileDropdown;
