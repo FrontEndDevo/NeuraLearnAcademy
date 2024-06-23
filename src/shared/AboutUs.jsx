d3c4e2bc257b4ec339c3cffd57823c45b36513d5
FrontEndDevo <mylinkacco@gmail.com>
2024-06-24 02:10:21 +0300

    Edit the styles of About and Contact components

--- src/shared/AboutUs.jsx
+++ src/shared/AboutUs.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ProjectTeam from "./ProjectTeam";
 
 const AboutUs = () => {
   return (
-    <section className="my-20 overflow-hidden bg-white sm:py-16">
-      <div className="px-6 mx-auto max-w-7xl lg:px-8">
-        <div className="grid max-w-2xl grid-cols-1 mx-auto gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+    <section className="my-32 overflow-hidden bg-white md:my-20 sm:py-16">
+      <div className="px-6 py-4 lg:px-8">
+        <div className="grid max-w-2xl grid-cols-1 justify-items-center gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
           <div className="lg:pr-8 lg:pt-4">
-            <div className="lg:max-w-lg">
+            <div className="text-center lg:max-w-lg">
               <h2 className="text-base font-semibold leading-7 text-indigo-600">
                 Empower Your Learning with Neural Learn Academy
               </h2>
@@ -18,7 +19,7 @@ const AboutUs = () => {
                 At Neural Learn Academy, we are dedicated to revolutionizing
                 education with our innovative platform.
               </p>
-              <dl className="max-w-xl mt-10 space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+              <dl className="max-w-xl mt-10 space-y-8 text-base text-gray-600 lg:max-w-none">
                 <div className="relative pl-9">
                   <dt className="inline font-semibold text-gray-900">
                     <svg
@@ -31,8 +32,8 @@ const AboutUs = () => {
                       <path d="M3.196 12.87l-.825.483a.75.75 0 000 1.294l7.25 4.25a.75.75 0 00.758 0l7.25-4.25a.75.75 0 000-1.294l-.825-.484-5.666 3.322a2.25 2.25 0 01-2.276 0L3.196 12.87z"></path>
                       <path d="M3.196 8.87l-.825.483a.75.75 0 000 1.294l7.25 4.25a.75.75 0 00.758 0l7.25-4.25a.75.75 0 000-1.294l-.825-.484-5.666 3.322a2.25 2.25 0 01-2.276 0L3.196 8.87z"></path>
                       <path d="M10.38 1.103a.75.75 0 00-.76 0l-7.25 4.25a.75.75 0 000 1.294l7.25 4.25a.75.75 0 00.76 0l7.25-4.25a.75.75 0 000-1.294l-7.25-4.25z"></path>
-                    </svg>
-                    Customized Learning Paths
+                    </svg>  
+                    Customized Learning Paths:{" "}
                   </dt>
                   <dd className="inline">
                     Explore personalized learning journeys tailored to your
@@ -54,7 +55,7 @@ const AboutUs = () => {
                         clipRule="evenodd"
                       ></path>
                     </svg>
-                    Cutting-edge Technology
+                    Cutting-edge Technology:{" "}
                   </dt>
                   <dd className="inline">
                     Utilize our advanced RESTful API to access, manage, and
@@ -76,7 +77,7 @@ const AboutUs = () => {
                       ></path>
                       <path d="M14.5 11.5c.173 0 .345-.007.514-.022l3.754 3.754a2.5 2.5 0 01-3.536 3.536l-4.41-4.41 2.172-2.607c.052-.063.147-.138.342-.196.202-.06.469-.087.777-.067.128.008.257.012.387.012zM6 4.586l2.33 2.33a.452.452 0 01-.08.09L6.8 8.214 4.586 6H3.309a.5.5 0 01-.447-.276l-1.7-3.402a.5.5 0 01.093-.577l.49-.49a.5.5 0 01.577-.094l3.402 1.7A.5.5 0 016 3.31v1.277z"></path>
                     </svg>
-                    Developer Friendly
+                    Developer Friendly:{" "}
                   </dt>
                   <dd className="inline">
                     Our platform is meticulously documented and easy to
@@ -97,12 +98,14 @@ const AboutUs = () => {
           <img
             src="https://images.unsplash.com/photo-1498050108023-c5249f4df085?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHw4fHxjb21wdXRlcnxlbnwwfDB8fHwxNjkxODE2NjY3fDA&ixlib=rb-4.0.3&q=80&w=1080"
             alt="Product screenshot"
-            className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+            className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 md:-ml-4 lg:-ml-0"
             width="2432"
             height="1442"
           />
         </div>
       </div>
+
+      <ProjectTeam />
     </section>
   );
 };
