b634d62a2e4cb21e3a92e4780156f0248125dcfe
FrontEndDevo <mylinkacco@gmail.com>
2024-06-24 15:41:24 +0300

    Refactor FeaturesSection component to update photo property names

--- src/components/FeaturesSection/FeaturesSection.jsx
+++ src/components/FeaturesSection/FeaturesSection.jsx
@@ -7,7 +7,7 @@ const professionalPeople = [
     story:
       '"I have been able to apply the skills I learned in the Professional Certificate to my work. I have been able to take on more responsibility and have been promoted to CEO of international. I am very grateful for the opportunity to learn and grow."',
     job: "CEO manager",
-    CardImage: "https://i.postimg.cc/L62WrtBL/John-Stowt.jpg",
+    photo: "https://i.postimg.cc/L62WrtBL/John-Stowt.jpg",
   },
   {
     id: 2,
@@ -15,8 +15,7 @@ const professionalPeople = [
     story:
       '"One of the best decisions I have made was to take the Professional Certificate. I have been able to apply the skills I learned in the Professional Certificate to my work. I have been able to take on more responsibility and have been promoted to CEO of international. I am very grateful for the opportunity to learn and grow."',
     job: "Front End Developer",
-    CardImage:
-      "https://i.postimg.cc/QC6nLvxn/pexels-simon-robben-55958-614810.jpg",
+    photo: "https://i.postimg.cc/QC6nLvxn/pexels-simon-robben-55958-614810.jpg",
   },
   {
     id: 3,
@@ -24,7 +23,7 @@ const professionalPeople = [
     story:
       '"Now I am a full stack developer. I have been able to apply the skills I learned in the Professional Certificate to my work. I have been able to take on more responsibility and have been promoted to CEO of international. I am very grateful for the opportunity to learn and grow."',
     job: "full stack Developer",
-    CardImage: "https://i.postimg.cc/x1d4d9Zd/Karim-Mahmoud.jpg",
+    photo: "https://i.postimg.cc/x1d4d9Zd/Karim-Mahmoud.jpg",
   },
 ];
 
@@ -35,29 +34,29 @@ const FeaturesSection = () => {
     backgroundRepeat: "no-repeat",
   };
 
-  const professionalCertifications = professionalPeople.map((card) => (
-    <div key={card.id} className="p-4">
-      <div className=" relative bg-black rounded-tl-[23px] rounded-tr-[23px] rounded-bl-[23px]">
-        <h2 className=" text-white text-[25px] md:text-[35px] text-opacity-80 font-extrabold ] tracking-widest pt-12 pb-20 pl-6 md:pl-20 select-none">
+  const professionalCertifications = professionalPeople.map((person) => (
+    <div key={person.id} className="p-4">
+      <div className="relative bg-black rounded-tl-[23px] rounded-tr-[23px] rounded-bl-[23px]">
+        <h2 className="text-white text-[25px] md:text-[35px] text-opacity-80 font-extrabold ] tracking-widest pt-12 pb-20 pl-6 md:pl-20 select-none">
           #Comments
         </h2>
         <h2 className="absolute text-2xl font-extrabold text-white capitalize bottom-2 left-36">
-          {card.title}
+          {person.title}
         </h2>
       </div>
 
       <div className="relative CardDetails bg-stone-5">
         <img
-          src={card.CardImage}
-          alt={card.title}
-          className="absolute left-0 z-10 w-32 h-32 text-white border-4 border-white rounded-full shadow-lg -top-20"
+          src={person.photo}
+          alt={person.title}
+          className="absolute left-0 z-10 w-32 h-32 mx-1 text-white border-4 border-white rounded-full shadow-lg -top-20"
         />
         <div className="absolute py-2 text-sm font-semibold left-32 md:left-36 text-neutral-500 md:text-normal">
-          {card.job}
+          {person.job}
         </div>
 
         <p className="h-full py-10 text-black md:text-[17px] px-6 pt-16 bg-stone-50">
-          {card.story}
+          {person.story}
         </p>
       </div>
     </div>
