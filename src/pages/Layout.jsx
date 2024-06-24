e10389fc03667ef29ea49e55379695d1b2caf632
FrontEndDevo <mylinkacco@gmail.com>
2024-06-24 13:16:11 +0300

    Update popups in Instructor and Layout pages

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,12 +1,21 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../common/Navbar/Navbar";
 import Footer from "../common/Footer/Footer";
+import { createPortal } from "react-dom";
+import Toast from "../shared/popup/Toast";
+import Spinner from "../shared/popup/Spinner";
+
+const popupsMessages = document.getElementById("popups");
 
 const Layout = () => {
   return (
     <>
+      {createPortal(<Toast />, popupsMessages)}
+      {createPortal(<Spinner />, popupsMessages)}
       <Navbar />
-      <Outlet />
+      <div className="my-40">
+        <Outlet />
+      </div>
       <Footer />
     </>
   );
