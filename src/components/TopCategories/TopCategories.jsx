9952152eb8af60ec93674c247a928b38c02e2b9f
FrontEndDevo <mylinkacco@gmail.com>
2024-06-24 12:20:41 +0300

    Update TopCategories component to handle empty subjectCourses array

--- src/components/TopCategories/TopCategories.jsx
+++ src/components/TopCategories/TopCategories.jsx
@@ -2,7 +2,8 @@ import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 function TopCategories() {
-  const allCategories = useSelector((state) => state.courses.subjectCourses);
+  const allCategories =
+    useSelector((state) => state.courses.subjectCourses) || [];
 
   const renderCategories = allCategories.map((item) => {
     return (
@@ -28,9 +29,11 @@ function TopCategories() {
   return (
     <>
       <div className="container my-10 xl:my-20 ">
-        <h3 className="mb-5 ml-3 text-3xl font-bold text-center xl:text-left">
-          Top Categories
-        </h3>
+        {allCategories.length !== 0 && (
+          <h3 className="mb-5 ml-3 text-3xl font-bold text-center xl:text-left">
+            Top Categories
+          </h3>
+        )}
         <ul className="grid grid-cols-2 gap-4 md:grid-cols-3 xl:grid-cols-5">
           {renderCategories}
         </ul>
