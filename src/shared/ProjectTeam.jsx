8725e0f6bd240ee913cb04e568fe72f9d99c366e
BasemYahia402 <basemyahia70@gmail.com>
2024-07-21 06:58:15 +0300

    Update name of team and style of card

--- src/shared/ProjectTeam.jsx
+++ src/shared/ProjectTeam.jsx
@@ -7,48 +7,51 @@ import khaled from "../assets/images/homepage/team/khaled.jpg";
 import zaytony from "../assets/images/homepage/team/zaytony.jpg";
 const ProjectTeam = () => {
     return (
-        <section id="our-team" className="bg-gray-100 py-32">
-            <div className="container mx-auto px-4">
-                <h2 className="text-3xl font-bold text-center mb-8 text-primary">Meet Our Team</h2>
+      <section id="our-team" className="py-32 bg-gray-100">
+        <div className="container px-4 mx-auto">
+          <h2 className="mb-8 text-3xl font-bold text-center text-primary">
+            Meet Our Team
+          </h2>
 
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-
-                    <TeamMember
-                        imgSrc={sherif}
-                        name="Peter Johnson"
-                        role="Machine Learning Developer"
-                    />
-                    <TeamMember
-                        imgSrc={badwy}
-                        name="Mohamed Ashraf "
-                        role="UX Designer"
-                    />
-                    <TeamMember
-                        imgSrc={khaled}
-                        name="Khaled Mohamed"
-                        role="Backend Developer"
-                    />
-                    <TeamMember
-                        imgSrc={zaytony}
-                        name="Mahmoud Ahmed"
-                        role="Backend Developer"
-                    />
-                    <TeamMember
-                        imgSrc={basem}
-                        name="Basem Yhia"
-                        role="Front End Developer "
-                    />
-                    <TeamMember
-                        imgSrc={besho}
-                        name="Beshoy Tag"
-                        role="Front End Developer " />
-                    <TeamMember
-                        imgSrc={karim}
-                        name="Karim Abdelazim"
-                        role="Front End Developer " />
-                </div>
-            </div>
-        </section>
+          <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-4">
+            <TeamMember
+              imgSrc={sherif}
+              name="sherif Mohamed"
+              role="Machine Learning Developer"
+            />
+            <TeamMember
+              imgSrc={badwy}
+              name="Mohamed Ashraf "
+              role="UX Designer"
+            />
+            <TeamMember
+              imgSrc={khaled}
+              name="Khaled Mohamed"
+              role="Backend Developer"
+            />
+            <TeamMember
+              imgSrc={zaytony}
+              name="Mahmoud Ahmed"
+              role="Backend Developer"
+            />
+            <TeamMember
+              imgSrc={basem}
+              name="Basem Yahia"
+              role="Front End Developer "
+            />
+            <TeamMember
+              imgSrc={besho}
+              name="Beshoy Tag"
+              role="Front End Developer "
+            />
+            <TeamMember
+              imgSrc={karim}
+              name="Karim Abdelazim"
+              role="Front End Developer "
+            />
+          </div>
+        </div>
+      </section>
     );
 };
 
@@ -59,7 +62,7 @@ const TeamMember = ({ imgSrc, name, role }) => {
       <img
         src={imgSrc}
         alt={name}
-        className="w-64 h-64 mb-4 ml-6 rounded-full"
+        className="w-64 h-64 mb-4 rounded-full"
       />
       <h3 className="mb-2 text-2xl font-semibold">{name}</h3>
       <p className="text-sm font-bold text-gray-700">{role}</p>
